e66db8b9cc9e4811a264ae2945671f21ca4671b1
Vladimir <justlikevova@gmail.com>
2023-03-06 22:11:42 +0300

    add corsheaders

--- backend/games/config/settings.py
+++ backend/games/config/settings.py
@@ -19,11 +19,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
