48593b9417fe75a642a5b4eed5e5ff09b7bc3b56
recoil <recoilvrn@yandex.ru>
2023-03-11 02:13:16 +0300

    [+] add model Product

--- backend/games/core/admin.py
+++ backend/games/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Product
+
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+# при регистрации модели Post источником конфигурации
+# для неё назначаем класс PostAdmin
+admin.site.register(Product, ProductAdmin)
--- backend/games/core/models.py
+++ backend/games/core/models.py
@@ -1,3 +1,50 @@
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Social(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField('Наименования соц. сети'
+                            ,max_length=100)
+
+class Product(models.Model):
+    class StatusProduct(models.TextChoices):
+        MODERATION = 'M', _('MODERATION')
+        PUBLISH = 'P', _('PUBLISH')
+        CLOSE = 'C', _('CLOSE')
+
+    id = models.UUIDField('UUID продукта'
+                          ,primary_key=True
+                          ,default = uuid.uuid4
+                          ,editable = False)
+    name = models.CharField('Наименование продукта'
+                            ,max_length=100
+                            ,help_text='Введите наименование продукта')
+    release_date = models.DateField('Дата релиза'
+                                    ,auto_now_add=True
+                                    ,db_index=True)
+    developers_uuid = models.UUIDField('UUID разработчика - service Users'
+                                       ,db_index=True)
+    publishers_uuid = models.UUIDField('UUID издателя - service Users'
+                                       ,db_index=True)
+    description = models.TextField('Описание продукта')
+    about = models.TextField('Описание продукта')
+    age = models.CharField('Возрастное ограничение'
+                           ,max_length=10)
+    adult = models.TextField
+    status = models.CharField(
+        max_length=2,
+        choices=StatusProduct.choices,
+        default=StatusProduct.MODERATION,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'товары'
