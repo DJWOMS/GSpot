883bb771e0e1bc128fd00eac242c808a857a7e93
Попов Никита <popovn@reshenie-soft.ru>
2023-03-11 18:45:27 +0300

    fix [+] Добавлена поле type [!] исправлено поле adult [!] изменен тип age

--- backend/games/core/models.py
+++ backend/games/core/models.py
@@ -10,30 +10,53 @@ class Product(models.Model):
         PUBLISH = 'P', _('PUBLISH')
         CLOSE = 'C', _('CLOSE')
 
-    id = models.UUIDField('UUID продукта'
-                          , primary_key=True
-                          , default=uuid.uuid4
-                          , editable=False)
-    name = models.CharField('Наименование продукта'
-                            , max_length=100
-                            , help_text='Введите наименование продукта')
-    release_date = models.DateField('Дата релиза'
-                                    , auto_now_add=True
-                                    , db_index=True)
-    developers_uuid = models.UUIDField('UUID разработчика - service Users'
-                                       , db_index=True)
-    publishers_uuid = models.UUIDField('UUID издателя - service Users'
-                                       , db_index=True)
-    description = models.TextField('Описание продукта')
-    about = models.TextField('Описание продукта')
-    age = models.CharField('Возрастное ограничение'
-                           , max_length=10)
-    adult = models.TextField
-    status = models.CharField(
-        max_length=2,
-        choices=StatusProduct.choices,
-        default=StatusProduct.MODERATION,
-    )
+    class TypeProduct(models.TextChoices):
+        GAMES = 'G', _('GAMES')
+        DLC = 'D', _('ADDITIONS')
+        SOUND = 'S', _('SOUNDS')
+
+    id = models.UUIDField('UUID продукта',
+                          primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+
+    name = models.CharField('Наименование продукта',
+                            max_length=100,
+                            help_text='Введите наименование продукта')
+
+    release_date = models.DateField('Дата релиза',
+                                    auto_now_add=True,
+                                    db_index=True)
+
+    developers_uuid = models.UUIDField('UUID разработчика - service Users',
+                                       db_index=True)
+
+    publishers_uuid = models.UUIDField('UUID издателя - service Users',
+                                       db_index=True)
+
+    description = models.TextField('Краткое описание',
+                                   help_text='Укажите краткое описание')
+
+    about = models.TextField('Об этой игре / информация о дополнении',
+                             help_text='Укажите описание')
+
+    age = models.IntegerField('Возрастное ограничение',
+                              blank=True,
+                              null=True)
+
+    adult = models.TextField('Описание возрастного ограничения',
+                             blank=True,
+                             null=True,)
+
+    status = models.CharField(max_length=2,
+                              choices=StatusProduct.choices,
+                              default=StatusProduct.MODERATION)
+
+    type = models.CharField(max_length=2,
+                            choices=TypeProduct.choices,
+                            blank=False,
+                            null=False,
+                            help_text='Укажите тип продукта')
 
     def __str__(self):
         return self.name
