cc7e7f9268785fe925b801dcdd3f6e1c92c46db6
bakdoolot-kg <altybaevbaku15@gmail.com>
2023-03-13 03:30:25 +0600

    Переместил classes в папку base

--- backend/games/core/views.py
+++ backend/games/core/views.py
@@ -1,8 +1,8 @@
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
-from . import classes
+from base import classes
 from . import serializers, models
 from .filters import ProductFilter
 
