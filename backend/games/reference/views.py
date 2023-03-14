8934bd97f762d31208d8b2b752f487f6ddccd959
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-03-14 14:39:27 +0700

    create view GroupElementView

--- backend/games/reference/views.py
+++ backend/games/reference/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, AllowAny
 from games.reference.serializers import GroupSerializer
 
+
 class GroupView(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
