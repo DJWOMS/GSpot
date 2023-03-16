63c0f6ea0375c87a38e1a52a6690723b1a98eb1e
KeirisTorn <Keiristorn@gmail.com>
2023-03-16 20:48:11 +0300

    добавлены модели, сделан аdmin

--- backend/games/reference/admin.py
+++ backend/games/reference/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+class GanreAdmin(admin.ModelAdmin):
+    class Meta():
+        madels = Ganre
+        
+class SubGanreAdmin (admin.ModelAdmin):
+    class Meta():
+        models = SubGanre
+        
+admin.site.register(Ganre, GanreAdmin)
+admin.site.register(SubGanre, SubGanreAdmin)
--- backend/games/reference/models.py
+++ backend/games/reference/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-from ..core.models import Product
+from core.models import Product
+
+class Ganre(models.Model):
+    name = models.CharField('категории', max_length=50, db_index=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+    class Meta():
+        verbose_name = 'категория для жанра'
+        verbose_name_plural = 'категории для жанров'
+        
+
+class SubGanre(models.Model):
+    name = models.CharField('sub ganre', max_length=50)
+    ganre_id = models.ForeignKey(Ganre, related_name='ganre', unique=True, on_delete=models.CASCADE)
+    product_id = models.ManyToManyField(Product, related_name='sub_ganre')
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+    class Meta():
+        verbose_name = 'жанр для игры'
+        verbose_name_plural = 'жанры для игр'
