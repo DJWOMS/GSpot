976adb9e7c7e70f548fd47fe82808dec94132734
Vladimir <justlikevova@gmail.com>
2023-03-17 13:12:48 +0300

    config | add redoc

--- backend/games/config/yasg.py
+++ backend/games/config/yasg.py
@@ -18,5 +18,5 @@ schema_view = get_schema_view(
 urlpatterns = [
    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
