73c4a91e3ac532c459c2edc2590a1dd4d677fb56
Попов Никита <popovn@reshenie-soft.ru>
2023-03-19 13:51:25 +0300

    [!] refactoring code test1

--- backend/games/core/models.py
+++ backend/games/core/models.py
@@ -14,48 +14,67 @@ class Product(models.Model):
         GAMES = 'G', _('GAMES')
         DLC = 'D', _('ADDITIONS')
 
-    id = models.UUIDField('UUID продукта',
-                          primary_key=True,
-                          default=uuid.uuid4,
-                          editable=False)
+    id = models.UUIDField('UUID продукта', primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Идентификатор записи ')
 
-    name = models.CharField('Наименование продукта',
-                            max_length=100,
-                            help_text='Введите наименование продукта')
+    name = models.CharField(
+        'Наименование продукта',
+        max_length=100,
+        help_text='Введите наименование продукта'
+    )
 
-    release_date = models.DateField('Дата релиза',
-                                    auto_now_add=True,
-                                    db_index=True)
+    release_date = models.DateField(
+        'Дата релиза',
+        auto_now_add=True,
+        db_index=True
+    )
 
-    developers_uuid = models.UUIDField('UUID разработчика - service Users',
-                                       db_index=True)
+    developers_uuid = models.UUIDField(
+        'UUID разработчика - service Users',
+        db_index=True
+    )
 
-    publishers_uuid = models.UUIDField('UUID издателя - service Users',
-                                       db_index=True)
+    publishers_uuid = models.UUIDField(
+        'UUID издателя - service Users',
+        db_index=True
+    )
 
-    description = models.TextField('Краткое описание',
-                                   help_text='Укажите краткое описание')
+    description = models.TextField(
+        'Краткое описание',
+        help_text='Укажите краткое описание'
+    )
 
-    about = models.TextField('Об этой игре / информация о дополнении',
-                             help_text='Укажите описание')
+    about = models.TextField(
+        'Об этой игре / информация о дополнении',
+        help_text='Укажите описание'
+    )
 
-    age = models.IntegerField('Возрастное ограничение',
-                              blank=True,
-                              null=True)
+    age = models.IntegerField(
+        'Возрастное ограничение',
+        blank=True,
+        null=True
+    )
 
-    adult = models.TextField('Описание возрастного ограничения',
-                             blank=True,
-                             null=True, )
+    adult = models.TextField(
+        'Описание возрастного ограничения',
+        blank=True,
+        null=True,
+    )
 
-    status = models.CharField(max_length=2,
-                              choices=StatusProduct.choices,
-                              default=StatusProduct.MODERATION)
+    status = models.CharField(
+        max_length=2,
+        choices=StatusProduct.choices,
+        default=StatusProduct.MODERATION
+    )
 
-    type = models.CharField(max_length=2,
-                            choices=TypeProduct.choices,
-                            help_text='Укажите тип продукта')
+    type = models.CharField(
+        max_length=2,
+        choices=TypeProduct.choices,
+        help_text='Укажите тип продукта'
+    )
 
-    dlcs = models.ManyToManyField('self', through="GameDlcLink", blank=True)
+    dlcs = models.ManyToManyField(
+        'self', through="GameDlcLink", blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -67,20 +86,26 @@ class Product(models.Model):
 
 
 class GameDlcLink(models.Model):
-    id = models.UUIDField('UUID продукта',
-                          primary_key=True,
-                          default=uuid.uuid4,
-                          editable=False)
+    id = models.UUIDField(
+        'UUID продукта',
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
 
-    game = models.ForeignKey(Product,
-                             on_delete=models.CASCADE,
-                             related_name='dlc_link',
-                             limit_choices_to={'type': Product.TypeProduct.GAMES})
+    game = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='dlc_link',
+        limit_choices_to={'type': Product.TypeProduct.GAMES}
+    )
 
-    dlc = models.ForeignKey(Product,
-                            on_delete=models.CASCADE,
-                            related_name='game_link',
-                            limit_choices_to={'type': Product.TypeProduct.DLC})
+    dlc = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='game_link',
+        limit_choices_to={'type': Product.TypeProduct.DLC}
+    )
 
     class Meta:
         db_table = 'game_dlc_link'
@@ -106,43 +131,56 @@ class SystemRequirement(models.Model):
         MINIMUM = 'M', _('Minimals')
         RECOMMEND = 'R', _('Recommends')
 
-    id = models.UUIDField('UUID продукта',
-                          primary_key=True,
-                          default=uuid.uuid4,
-                          editable=False)
+    id = models.UUIDField(
+        'UUID продукта',
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
 
-    operating_system = models.CharField('ОС',
-                                        max_length=2,
-                                        choices=OS.choices,
-                                        help_text='Укажите ОС')
+    operating_system = models.CharField(
+        'ОС',
+        max_length=2,
+        choices=OS.choices,
+        help_text='Укажите ОС'
+    )
 
-    game = models.ForeignKey(Product,
-                             on_delete=models.CASCADE,
-                             related_name='system_requirements',
-                             limit_choices_to={
-                                 'type': Product.TypeProduct.GAMES})
+    game = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='system_requirements',
+        limit_choices_to={
+            'type': Product.TypeProduct.GAMES}
+    )
 
-    device_processor = models.CharField('Процессор',
-                                        max_length=100,
-                                        help_text='Укажите процессор')
+    device_processor = models.CharField(
+        'Процессор',
+        max_length=100,
+        help_text='Укажите процессор'
+    )
 
-    device_memory = models.CharField('Количество ОЗУ',
-                                     max_length=100,
-                                     help_text='Укажите количество ОЗУ')
+    device_memory = models.CharField(
+        'Количество ОЗУ',
+        max_length=100,
+        help_text='Укажите количество ОЗУ'
+    )
 
-    device_storage = models.CharField('Количество памяти на Диске',
-                                      max_length=100,
-                                      help_text='Укажите количество памяти на Диске')
+    device_storage = models.CharField(
+        'Количество памяти на Диске',
+        max_length=100,
+        help_text='Укажите количество памяти на Диске'
+    )
 
     device_graphics = models.CharField(
         'Модель видеокарты и количество памяти',
         max_length=100,
         help_text='Укажите модель видеокарты и количество памяти')
 
-    type_requirements = models.CharField('Тип системных требований',
-                                         max_length=2,
-                                         choices=TypeRequirements.choices,
-                                         help_text='Укажите тип системных требований')
+    type_requirements = models.CharField(
+        'Тип системных требований',
+        max_length=2,
+        choices=TypeRequirements.choices,
+        help_text='Укажите тип системных требований'
+    )
 
     def __str__(self):
         return self.type_requirements
