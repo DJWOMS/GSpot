70316c01ceaa454b1cb3fba2de85f2b0549710b6
kieled <68655454+kieled@users.noreply.github.com>
2023-03-23 01:24:24 +0300

    FRONTEND | Sync mock data with backend (#49)
    * sync and fixed
    
    * sync and fixed bugs
    
    * Delete yarn.lock
    
    * removed extra added missed
    
    * partially migrated to styled components. Project doesn't starting
    
    * fully switched to tailwind
    
    * end css modules migration
    
    * added mocks data
    
    * sync mock data with backendr's
    
    * updated intefraces for new mock data

--- frontend/features/games/models/IProduct.ts
+++ frontend/features/games/models/IProduct.ts
@@ -1,14 +1,8 @@
-import { URL, UUID, DATE } from '@defaultTypes'
+import { UUID, DATE } from '@defaultTypes'
 
 export interface IProductGenre {
     id: UUID
     name: string
-}
-
-export interface IProductPlatform {
-    id: UUID
-    name: string
-    icon: URL
 }
 
 export interface IProduct {
@@ -16,9 +10,53 @@ export interface IProduct {
     name: string
     releaseDate: DATE
     genres: IProductGenre[]
-    platforms: IProductPlatform[]
+    systemRequirements: {
+        id: UUID
+        operatingSystem: string
+    }[]
     price: number
     discount?: number
-    is_bought: boolean
-    is_favorite: boolean
+    isBought: boolean
+    isFavorite: boolean
+}
+
+export interface IProductLang {
+    id: UUID
+    languageName: string
+    interface: boolean
+    subtitles: boolean
+    voice: boolean
+}
+
+export interface IProductDlc {
+    id: UUID
+    name: string
+    description: string
+    developersUuid: UUID
+    publishersUuid: UUID
+    langs: IProductLang[]
+}
+
+export interface IProductSystemRequirement {
+    id: UUID
+    operatingSystem: string
+    deviceProcessor: string
+    deviceMemory: string
+    deviceStorage: string
+    deviceGraphics: string
+    typeRequirements: string
+}
+
+export interface IProductDetail extends IProduct {
+    description: string
+    about: string
+    age: number | null
+    adult: string
+    status: string
+    type: string
+    developersUuid: UUID
+    publishersUuid: UUID
+    dlcs: IProductDlc[]
+    langs: IProductLang[]
+    systemRequirements: IProductSystemRequirement[]
 }
