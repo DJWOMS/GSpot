d57070bd8ae516f423acb7783c1d94215bfadc9e
Vladimir <justlikevova@gmail.com>
2023-03-23 18:20:19 +0300

    - fix file to max-line-length=100

--- backend/games/reference/views.py
+++ backend/games/reference/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAdminUser
 from .models import Language, ProductLanguage, Genre, SubGenre
-from .serializers import LanguageSerializer, ProductLanguageSerializer, GenreSerializer, SubGenreSerializer
+from .serializers import (
+    LanguageSerializer,
+    ProductLanguageSerializer,
+    GenreSerializer,
+    SubGenreSerializer
+)
 
 
 class LanguageView(viewsets.ModelViewSet):
