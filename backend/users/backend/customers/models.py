48d87ea28b2a940aa367cfbd1426a307201513c2
Romver <roma_zverev2002@mail.ru>
2023-03-24 19:43:07 +0300

    add customer models

--- backend/users/backend/customers/models.py
+++ backend/users/backend/customers/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from admins.models import Country
+from common.models import BaseAbstractUser
+
+
+class CustomerUser(BaseAbstractUser):
+    friends = models.ManyToManyField(
+        'self',
+        symmetrical=True,
+        through='FriendShipRequest',
+        through_fields=('sender', 'receiver')
+    )
+    phone_number = models.CharField(
+        verbose_name='Номер телефона',
+        blank=True,
+        max_length=15,
+        null=True)
+    # image = models.ImageField(  # Где храним?
+    #     verbose_name='Фото',
+    #     upload_to='media/users')
+    country = models.ForeignKey('Country', verbose_name='Страна',
+                                on_delete=models.SET_NULL,
+                                null=True)
+
+    class Meta:
+        db_table = 'customer'
+
+
+class FriendShipRequest(models.Model):
+    REQUESTED = 'REQUESTED'  # запрошен
+    ACCEPTED = 'ACCEPTED'    # принято
+    REJECTED = 'REJECTED'    # отклонен
+
+    STATUS_CHOICES = (
+        (REQUESTED, 'requested'),
+        (ACCEPTED, 'accepted'),
+        (REJECTED, 'rejected')
+    )
+    status = models.TextField(choices=STATUS_CHOICES, default=REQUESTED)
+    sender = models.ForeignKey(CustomerUser, on_delete=models.CASCADE, related_name='sender')      # отправитель
+    receiver = models.ForeignKey(CustomerUser, on_delete=models.CASCADE, related_name='receiver')  # получатель
