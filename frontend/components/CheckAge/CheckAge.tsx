848e62141d1016aa14bffd23d08e07ea5bb10cec
Katetcpip <katetcpip@yabdex.ru>
2023-03-24 15:14:41 +0300

    fixes css.modules

--- frontend/components/CheckAge/CheckAge.tsx
+++ frontend/components/CheckAge/CheckAge.tsx
@@ -1,29 +1,62 @@
-import './styles.css'
-import Bday from './Bday'
+import styles from './CheckAge.module.scss'
+import cn from 'classnames'
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+const Bday = (): JSX.Element => {
+    const [startDay, setStartDay] = useState(new Date())
+    const [starMonth, setStarMonth] = useState(new Date())
+    const [startYear, setStartYear] = useState(new Date())
+    return (
+        <div className={styles.divDatePicker}>
+            <DatePicker
+                className={cn(styles.bday, 'lg:w-10 w-8 lg:text-xl text-xs')}
+                selected={startDay}
+                onChange={(date: Date) => setStartDay(date)}
+                dateFormat="dd"
+            />
+            <DatePicker
+                className={cn(styles.bday, 'lg:w-24 w-14')}
+                selected={starMonth}
+                onChange={(date: Date) => setStarMonth(date)}
+                showMonthYearPicker
+                dateFormat="MMMM"
+            />
+            <DatePicker
+                className={cn(styles.bday, 'lg:w-20 w-10')}
+                selected={startYear}
+                onChange={(date: Date) => setStartYear(date)}
+                showYearPicker
+                dateFormat="yyyy"
+            />
+        </div>
+    )
+}
 
 const CheckAge = () => {
     return (
-        <div className="mainBlock">
-            <div className="divStyle lg:p-44 lg:pb-10 pb-10 p-2">
-                <div className="md:h-1/3 h-1/4 lg:w-1/4 w-2/4 absolute -top-10">
-                    <img alt="Game image" src="https://picsum.photos/1000" className="styleImg" />
+        <div className={styles.mainBlock}>
+            <div className={cn(styles.divStyle, 'lg:p-44 lg:pb-10 pb-10 p-2')}>
+                <div className={cn('md:h-1/3 h-1/4 lg:w-1/4 w-2/4', styles.divImg)}>
+                    <img alt="Game image" src="https://picsum.photos/1000" className={styles.styleImg} />
                 </div>
-                <div className="flex flex-col lg:w-4/5 w-11/12 h-full items-center justify-center contain ">
-                    <h1 className="styleH1 md:text-xl text-xs lg:pt-4 pt-10">
+                <div className={cn(styles.divH1, 'lg:w-4/5 w-11/12')}>
+                    <h1 className={cn(styles.styleH1, 'md:text-xl text-xs lg:pt-4 pt-10')}>
                         ВНИМАНИЕ: ИГРА МОЖЕТ СОДЕРЖАТЬ КОНТЕНТ, НЕ ПОДХОДЯЩИЙ ДЛЯ ВСЕХ ВОЗРАСТОВ ИЛИ ДЛЯ ПРОСМОТРА НА РАБОТЕ.
                     </h1>
-                    <form className="styleForm md:text-xl text-xs">
-                        <p>Пожалуйста, укажите дату своего рождения:</p>
+                    <form className={cn(styles.styleForm, 'md:text-xl text-xs')}>
+                        <p className={styles.p}>Пожалуйста, укажите дату своего рождения:</p>
                         <Bday />
                     </form>
-                    <div className="flex flex-row gap-3 w-full justify-center mt-6">
-                        <button className="btn lg:text-xl text-xs">Открыть страницу</button>
-                        <button className="btn lg:text-xl text-xs">Отмена</button>
+                    <div className={styles.divBtn}>
+                        <button className={cn(styles.btn, 'lg:text-xl text-xs')}>Открыть страницу</button>
+                        <button className={cn(styles.btn, 'lg:text-xl text-xs')}>Отмена</button>
                     </div>
                 </div>
             </div>
-            <footer className="footer">
-                <p className="md:text-xl text-xs">Эта информация предназначена исключительно для проверки и не будет сохранена.</p>
+            <footer className={styles.footer}>
+                <p className={cn(styles.p, 'md:text-xl text-xs')}>Эта информация предназначена исключительно для проверки и не будет сохранена.</p>
             </footer>
         </div>
     )
