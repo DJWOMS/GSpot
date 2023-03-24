7ac90f088111077d24fa7caed5dd2b3dd0269fa1
Katetcpip <katetcpip@yabdex.ru>
2023-03-24 15:26:51 +0300

    all in CheckAge now

--- frontend/components/CheckAge/CheckAge.tsx
+++ frontend/components/CheckAge/CheckAge.tsx
@@ -4,37 +4,11 @@ import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-const Bday = (): JSX.Element => {
+const CheckAge = () => {
     const [startDay, setStartDay] = useState(new Date())
     const [starMonth, setStarMonth] = useState(new Date())
     const [startYear, setStartYear] = useState(new Date())
-    return (
-        <div className={styles.divDatePicker}>
-            <DatePicker
-                className={cn(styles.bday, 'lg:w-10 w-8 lg:text-xl text-xs')}
-                selected={startDay}
-                onChange={(date: Date) => setStartDay(date)}
-                dateFormat="dd"
-            />
-            <DatePicker
-                className={cn(styles.bday, 'lg:w-24 w-14')}
-                selected={starMonth}
-                onChange={(date: Date) => setStarMonth(date)}
-                showMonthYearPicker
-                dateFormat="MMMM"
-            />
-            <DatePicker
-                className={cn(styles.bday, 'lg:w-20 w-10')}
-                selected={startYear}
-                onChange={(date: Date) => setStartYear(date)}
-                showYearPicker
-                dateFormat="yyyy"
-            />
-        </div>
-    )
-}
 
-const CheckAge = () => {
     return (
         <div className={styles.mainBlock}>
             <div className={cn(styles.divStyle, 'lg:p-44 lg:pb-10 pb-10 p-2')}>
@@ -47,7 +21,29 @@ const CheckAge = () => {
                     </h1>
                     <form className={cn(styles.styleForm, 'md:text-xl text-xs')}>
                         <p className={styles.p}>Пожалуйста, укажите дату своего рождения:</p>
-                        <Bday />
+
+                        <div className={styles.divDatePicker}>
+                            <DatePicker
+                                className={cn(styles.bday, 'lg:w-10 w-8 lg:text-xl text-xs')}
+                                selected={startDay}
+                                onChange={(date: Date) => setStartDay(date)}
+                                dateFormat="dd"
+                            />
+                            <DatePicker
+                                className={cn(styles.bday, 'lg:w-24 w-14')}
+                                selected={starMonth}
+                                onChange={(date: Date) => setStarMonth(date)}
+                                showMonthYearPicker
+                                dateFormat="MMMM"
+                            />
+                            <DatePicker
+                                className={cn(styles.bday, 'lg:w-20 w-10')}
+                                selected={startYear}
+                                onChange={(date: Date) => setStartYear(date)}
+                                showYearPicker
+                                dateFormat="yyyy"
+                            />
+                        </div>
                     </form>
                     <div className={styles.divBtn}>
                         <button className={cn(styles.btn, 'lg:text-xl text-xs')}>Открыть страницу</button>
