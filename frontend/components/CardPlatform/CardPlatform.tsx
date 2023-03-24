57b39ec2880056e47b92862b7bd10f97e43e219e
paqstd <zebartcoc@gmail.com>
2023-03-24 16:58:10 +0300

    Small fix for import from features

--- frontend/components/CardPlatform/CardPlatform.tsx
+++ frontend/components/CardPlatform/CardPlatform.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
+import { PlatformType } from 'features/games'
 import s from './CardPlatform.module.scss'
 import cn from 'classnames'
-import { PlatformType } from 'features/games/types'
 
 const CardPlatform: FC<PlatformType> = ({ type }): JSX.Element => {
   return <div className={cn(s.platform, s[type])} />
