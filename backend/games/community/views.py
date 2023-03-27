d7afaa155548514ef8f3a25044fed03413f42549
bogdan_vvv <bartmen1300@gmail.com>
2023-03-27 22:24:53 +0300

    Games | create view and urls for Reviews

--- backend/games/community/views.py
+++ backend/games/community/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from .models import Media
-from .serializers import MediaSerializer
+from .models import Media, Reviews
+from .serializers import MediaSerializer, ReviewsSerializer
 
 
 class MediaViewSet(viewsets.ModelViewSet):
     queryset = Media.objects.all()
     serializer_class = MediaSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
