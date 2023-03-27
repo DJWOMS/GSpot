dd2fce52a0b76507f7eebd45f79c37bd947a0b28
bogdan_vvv <bartmen1300@gmail.com>
2023-03-27 23:26:35 +0300

    Games |  urls for ReviewAnswers

--- backend/games/community/urls.py
+++ backend/games/community/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MediaViewSet, ReviewViewSet
+from .views import MediaViewSet, ReviewsViewSet, ReviewAnswersViewSet
 
 router = routers.DefaultRouter()
 router.register(r'media', MediaViewSet)
-router.register(r'reviews', ReviewViewSet)
+router.register(r'reviews', ReviewsViewSet)
+router.register(r'review_answers', ReviewAnswersViewSet)
 
 
 urlpatterns = [
