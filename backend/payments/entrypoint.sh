687834f0a28eefb564c8a4b7c2c3b3ef1e723102
fenx4 <127792143+fenx4@users.noreply.github.com>
2023-03-29 03:01:26 +0300

    Payments | docker multistage slim
    change base image alpine >> slim
    add to README env arguments for postgresql db,
    celery args right or not?
    about entrypoint scripts and start sh will discuss on meet

--- backend/payments/entrypoint.sh
+++ backend/payments/entrypoint.sh
@@ -1,16 +1,54 @@
-#!bin/sh
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+#!/usr/bin/env sh
+set -o errexit 
+set -o nounset
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+postgres_ready() {
+  python << END
+import sys
 
-    echo "PostgreSQL started"
-fi
+from psycopg2 import connect  
+from psycopg2.errors import OperationalError
 
-python manage.py makemigrations
+try:  
+  connect(  
+        dbname="${POSTGRES_DB}",  
+        user="${POSTGRES_USER}",  
+        password="${POSTGRES_PASSWORD}",  
+        host="${POSTGRES_HOST}",  
+        port="${POSTGRES_PORT}",  
+    )  
+except OperationalError:  
+    sys.exit(-1)  
+END
+}
+
+redis_ready() {
+    python << END
+import sys
+from redis import Redis
+from redis import RedisError
+try:
+    redis = Redis.from_url("${CELERY_BROKER_URL}", db=0)
+    redis.ping()
+except RedisError:
+    sys.exit(-1)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Waiting for PostgreSQL to become available..."  
+  sleep 5  
+done
+>&2 echo "PostgreSQL is available"
+
+until redis_ready; do
+  >&2 echo "Waiting for Redis to become available..."
+  sleep 5
+done
+>&2 echo "Redis is available"
+
+python manage.py collectstatic --noinput  
+python manage.py makemigrations  --noinput 
 python manage.py migrate
 
 exec "$@"
