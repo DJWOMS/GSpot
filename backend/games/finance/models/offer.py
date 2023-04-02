d95d1c9bf0ba4fbf8c0d52f72cd0eb345c585a03
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-04-02 17:40:02 +0700

    create models for finance/offer

---
+++ backend/games/finance/models/offer.py
@@ -0,0 +1,79 @@
+from base.choices import CurrencyChoices
+from base.model_fields import AmountField, get_field_from_choices
+from core.models import Product
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
+
+
+def correct_date(from_dttm, to_dttm):
+    if from_dttm < timezone.now() or from_dttm >= to_dttm:
+        raise ValidationError(
+            {'from_dttm': "from_dttm should more than current time and less than to_dttm"})
+
+
+class PriceAbstractModel(models.Model):
+    amount = AmountField('Price')
+    currency = get_field_from_choices("Currency", CurrencyChoices, default=CurrencyChoices.RUB)
+    created_by = models.UUIDField('Created by')
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Price(PriceAbstractModel):
+    updated_by = models.UUIDField('Updated by')
+    updated_at = models.DateTimeField('Updated at', auto_now=True)
+
+    class Meta:
+        db_table = "price"
+
+
+class SchedulerPrice(PriceAbstractModel):
+    price = models.ForeignKey(Price, on_delete=models.CASCADE, related_name='scheduler_prices')
+    from_dttm = models.DateTimeField('From dttm')
+    to_dttm = models.DateTimeField('To dttm')
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "scheduler_price"
+
+    def clean(self):
+        super().clean()
+        correct_date(self.from_dttm, self.to_dttm)
+
+
+class Offer(models.Model):
+    created_by = models.UUIDField('UUID creator', unique=True)
+    price = models.OneToOneField(Price, on_delete=models.CASCADE, related_name='offer')
+    is_active = models.BooleanField(default=False)
+    products = models.ManyToManyField(Product, through='ProductOffer', related_name='offers')
+
+    class Meta:
+        db_table = "offer"
+
+
+class ProductOffer(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    offer_id = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    created_by = models.UUIDField('UUID creator')
+    created_at = models.DateTimeField('Created At', auto_now_add=True)
+
+
+class Sale(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name='sales')
+    from_dttm = models.DateTimeField('From dttm')
+    to_dttm = models.DateTimeField('To dttm')
+    discount = models.PositiveSmallIntegerField(
+        'Discount', validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "sale"
+
+    def clean(self):
+        super().clean()
+        correct_date(self.from_dttm, self.to_dttm)
