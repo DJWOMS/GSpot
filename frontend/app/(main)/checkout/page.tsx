a58f691c814f5834430d526cccd2198431ff692e
smnov <aleksandr.semenovsky@gmail.com>
2023-04-05 09:57:33 +0800

    Frontend | Checkout css file and CheckoutGameCard component added

--- frontend/app/(main)/checkout/page.tsx
+++ frontend/app/(main)/checkout/page.tsx
@@ -1,104 +1,45 @@
 import { GameCardInterface } from 'features/games'
+import { CheckoutGameCard } from 'features/games/components/CheckoutGameCard/CheckoutGameCard'
 import { fetchServerSide } from 'lib/fetchServerSide'
-import Image from 'next/image'
 import Link from 'next/link'
+import s from './page.module.scss'
 
 const Checkout = async () => {
   const data = await fetchServerSide<GameCardInterface[]>({
     path: '/games/cart',
   })
   return (
-    <>
-      <div className="container mx-auto py-16">
-        <div className="mx-auto w-full max-w-2xl">
-          <div className="overflow-hidden rounded-lg bg-gray-800 shadow-lg">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold text-white">Your Order:</div>
-              <div className="text-white">
-                <div>
-                  {data?.map(({ title, coverImg, price, link, currency, sale }, id) => (
-                    <div className="mx-4 flex justify-between" key={id}>
-                      <div className="justify-normal flex">
-                        <Link href="/details/id">
-                          <Image src={coverImg} width={50} height={50} alt="" />
-                        </Link>
-                        <h3>
-                          <Link href={link}>{title}</Link>
-                        </h3>
-                      </div>
-
-                      <div>
-                        {currency}
-                        {sale ? sale : price}
-                        {sale && (
-                          <s>
-                            {currency}
-                            {price}
-                          </s>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="mb-2 flex justify-between">
-                  <div>Subtotal:</div>
-                  <div>RUB 60.00</div>
-                </div>
-                <div className="flex justify-between">
-                  <div>Total:</div>
-                  <div>RUB 60.00</div>
-                </div>
-              </div>
-            </div>
-            <div className="px-6 py-4">
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="name"
-                type="text"
-                placeholder="Name"
-              />
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="email"
-                type="text"
-                placeholder="Email"
-              />
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="address"
-                type="text"
-                placeholder="Address"
-              />
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="city"
-                type="text"
-                placeholder="City"
-              />
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="state"
-                type="text"
-                placeholder="State"
-              />
-              <input
-                className="mt-4 w-full appearance-none rounded border-2 border-gray-700 bg-gray-700 py-2 px-4 leading-tight text-white focus:border-gray-500 focus:bg-gray-600 focus:outline-none"
-                id="zip"
-                type="text"
-                placeholder="Zip"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <Link href="/success">
-                <button className="focus:shadow-outline rounded bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-600 focus:outline-none">
-                  Place Order
-                </button>
-              </Link>
-            </div>
+    <div className={s.checkoutWrapper}>
+      <div className={s.checkoutCover}>
+        <div className="px-6 py-4">
+          <div className={s.checkoutText}>Your Order:</div>
+          {data?.map((game, id) => (
+            <CheckoutGameCard {...game} key={id} />
+          ))}
+          <div className="flex justify-between">
+            <div>Subtotal:</div>
+            <div>RUB 60.00</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Total:</div>
+            <div>RUB 60.00</div>
           </div>
         </div>
+        <div className="px-6 py-4">
+          <input className={s.checkoutInput} id="name" type="text" placeholder="Name" />
+          <input className={s.checkoutInput} id="email" type="text" placeholder="Email" />
+          <input className={s.checkoutInput} id="address" type="text" placeholder="Address" />
+          <input className={s.checkoutInput} id="city" type="text" placeholder="City" />
+          <input className={s.checkoutInput} id="state" type="text" placeholder="State" />
+          <input className={s.checkoutInput} id="zip" type="text" placeholder="Zip" />
+        </div>
+        <div className="px-6 py-4">
+          <Link href="/success">
+            <button className={s.checkoutButton}>Place Order</button>
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
