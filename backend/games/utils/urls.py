f3b828b95b492c0d788776906a965999acbd2dc9
RasulMakhmudov <makhmudov_ra@vk.com>
2023-04-07 19:16:30 +0300

    [ADD] use list api view instead of modelviewset

--- backend/games/utils/urls.py
+++ backend/games/utils/urls.py
@@ -4,18 +4,24 @@ from rest_framework.routers import DefaultRouter
 
 from utils.views import (
     GetOperatingSystemListView,
-    GetMinMaxPriceApiView,
-    GetGenreViewSet,
-    GetSubGenreViewSet,
+    GetGenreListView,
+    GetMinMaxPriceListView,
+    GetSubGenreListView,
 )
 
 
 api_router = DefaultRouter()
-api_router.register("filters/genres", GetGenreViewSet, basename="all_genres")
-api_router.register("filters/sub_genres", GetSubGenreViewSet, basename="all_sub_genres")
 
 urlpatterns = [
     path("", include(api_router.urls)),
-    path("filters/platforms", GetOperatingSystemListView.as_view(), name="get_all_platforms"),
-    path("filters/prices", GetMinMaxPriceApiView.as_view(), name="get_min_max_price"),
+    path("filters/genres", GetGenreListView.as_view(), name="all_genres"),
+    path(
+        "filters/platforms", GetOperatingSystemListView.as_view(), name="all_platforms"
+    ),
+    path("filters/prices", GetMinMaxPriceListView.as_view(), name="min_max_price"),
+    path(
+        "filters/genres/<int:genre_id>/subgenres/",
+        GetSubGenreListView.as_view(),
+        name="subgenre",
+    ),
 ]
