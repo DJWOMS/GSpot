9f440f34424bd1564178639fe18f9e2fcaae5231
Kiel Ed <kieledssh@gmail.com>
2023-04-08 10:19:54 +0300

    try again, but no result

--- backend/channels/src/connectors/websocket.py
+++ backend/channels/src/connectors/websocket.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import WebSocket, WebSocketDisconnect
 import redis.asyncio as redis
 
@@ -20,16 +22,28 @@ class ConnectionManager:
     async def subscribe(self, websocket: WebSocket):
         try:
             async with self.connection.pubsub() as pubsub:
-                while True:
-                    await pubsub.psubscribe('channel:*')
+                await pubsub.psubscribe('channel:*')
+
+                async def wait_websocket():
                     while True:
                         message = await websocket.receive_text()
+                        await pubsub.publish('channel:*', message)
                         await websocket.send_text(f"Message text was: {message}")
+
+                async def wait_pubsub():
+                    while True:
                         message = await pubsub.get_message(ignore_subscribe_messages=True)
                         if message:
                             channel = message['channel'].decode()
                             data = message['data'].decode()
                             await websocket.send_text(f'Channel: {channel}, Message: {data}')
+
+                # Когда мы передаем управлению потоку, то соединение сразу же обрывается, при этом нужно держать сразу 2 бесконечных await
+
+                await asyncio.wait([
+                    wait_pubsub(),
+                    wait_websocket()
+                ])
 
         except WebSocketDisconnect:
             print('client disconnected')
