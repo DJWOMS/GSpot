185ca95997f3f5c8d7b1ca0fbefd3478fe85b5e8
lsoulworkerl <prosto2002vitalik@gmail.com>
2023-04-10 00:01:07 +0300

    Update migrations and readme

--- backend/payments/apps/payment_accounts/migrations/0002_rename_user_uid_account_user_uuid_and_more.py
+++ backend/payments/apps/payment_accounts/migrations/0002_rename_user_uid_account_user_uuid_and_more.py
@@ -7,41 +7,37 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("payment_accounts", "0001_initial"),
+        ('payment_accounts', '0001_initial'),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name="account",
-            old_name="user_uid",
-            new_name="user_uuid",
+            model_name='account',
+            old_name='user_uid',
+            new_name='user_uuid',
         ),
         migrations.AlterField(
-            model_name="account",
-            name="balance",
+            model_name='account',
+            name='balance',
             field=apps.base.fields.MoneyField(
                 decimal_places=2,
                 default=0,
                 max_digits=11,
                 validators=[
-                    django.core.validators.MinValueValidator(
-                        0, message="Insufficient Funds"
-                    )
+                    django.core.validators.MinValueValidator(0, message='Insufficient Funds'),
                 ],
             ),
         ),
         migrations.AlterField(
-            model_name="balancechange",
-            name="amount",
+            model_name='balancechange',
+            name='amount',
             field=apps.base.fields.MoneyField(
                 decimal_places=2,
                 default=0,
                 editable=False,
                 max_digits=11,
                 validators=[
-                    django.core.validators.MinValueValidator(
-                        0, message="Should be positive value"
-                    )
+                    django.core.validators.MinValueValidator(0, message='Should be positive value'),
                 ],
             ),
         ),
