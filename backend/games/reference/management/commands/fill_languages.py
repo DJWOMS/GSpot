dd8fa8d25f7ed557c5f10a1c9ccab92e43bf0ab7
bogdan_vvv <bartmen1300@gmail.com>
2023-04-10 20:56:57 +0300

    Games | test data for Languages

--- backend/games/reference/management/commands/fill_languages.py
+++ backend/games/reference/management/commands/fill_languages.py
@@ -8,33 +8,29 @@ import random
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if Language.objects.filter(name__contains='test').exists():
-            self.stdout.write('Test data Language already exists')
-            return
-        Language.objects.bulk_create([
-            Language(name='English_test'),
-            Language(name='Spanish_test'),
-            Language(name='French_test'),
-        ])
-        self.stdout.write('Successfully created test data for Language')
+        if not Language.objects.filter(name__contains='test').exists():
+            Language.objects.bulk_create([
+                Language(name='English_test'),
+                Language(name='Spanish_test'),
+                Language(name='French_test'),
+            ])
+            self.stdout.write('Successfully created test data for Language')
 
         if not Product.objects.exists():
-            call_command('python manage.py fill_products')
+            call_command('fill_products', 3)
 
         language = Language.objects.all()
         products = Product.objects.all()
         choice = (False, True)
 
-        if ProductLanguage.objects.exists():
-            self.stdout.write('Test data ProductLanguage already exists')
-            return
-        for product in products:
-            ProductLanguage.objects.create(
-                language=random.choice(language),
-                product=product,
-                interface=random.choice(choice),
-                subtitles=random.choice(choice),
-                voice=random.choice(choice)
-            )
+        if not ProductLanguage.objects.exists():
+            for product in products:
+                ProductLanguage.objects.create(
+                    language=random.choice(language),
+                    product=product,
+                    interface=random.choice(choice),
+                    subtitles=random.choice(choice),
+                    voice=random.choice(choice)
+                )
 
         self.stdout.write('Successfully created test data for ProductLanguage')
