eed99f53f8e26c3b0f0691c6b2e6d18b8e000e24
Nikolay <67255397+NikolayN-ru@users.noreply.github.com>
2023-04-10 18:52:17 +0400

    Frontend | Profile pages settings/purchases (#84)
    * component Profile
    
    * describe component <Profile />
    
    * Frontend | page Profile settings/purchases + styles
    
    * Delete user.svg
    
    * Frontend | page Profile settings/purchases + styles
    
    * front-pull-5
    
    * attempt-6
    
    * Delete home.svg
    
    * delete/module 2 strok
    
    * 8
    
    * 8
    
    * Some fixes for styles & components
    
    * Fixed mocks interface for purchases, added new interface
    
    ---------
    
    Co-authored-by: kieled <68655454+kieled@users.noreply.github.com>
    Co-authored-by: Pavel Kutsenko <zebartcoc@gmail.com>

---
+++ frontend/app/(main)/profile/purchases/page.tsx
@@ -0,0 +1,34 @@
+import Pagination from 'components/Pagination'
+import { Purchase, PurchaseCardInterface } from 'features/profile'
+import { fetchServerSide } from 'lib/fetchServerSide'
+import s from './page.module.scss'
+
+const PurchasesItem = async () => {
+  const purhases = await fetchServerSide<PurchaseCardInterface[]>({
+    path: '/profile/purchases',
+  })
+
+  return (
+    <>
+      <table className={s.purchases}>
+        <thead>
+          <tr>
+            <th>№</th>
+            <th>Product</th>
+            <th>Title</th>
+            <th>Platform</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{purhases && purhases.map((purchase) => <Purchase key={purchase.title} {...purchase} />)}</tbody>
+      </table>
+
+      <Pagination />
+    </>
+  )
+}
+
+export default PurchasesItem
