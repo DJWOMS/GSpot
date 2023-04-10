ddf8de07be9421d95a6f2c4183816437616ac172
Pavel Kutsenko <zebartcoc@gmail.com>
2023-04-10 04:48:58 +0300

    Small refactoring for skeleton components

--- frontend/components/Skeleton/SkeletonListCheckBoxes/SkeletonListCheckBoxes.tsx
+++ frontend/components/Skeleton/SkeletonListCheckBoxes/SkeletonListCheckBoxes.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import cn from 'classnames'
+import { SkeletonCheckBox } from 'components/Skeleton'
 import s from './SkeletonListCheckBoxes.module.scss'
 
 interface SkeletonListCheckBoxesProps {
@@ -10,14 +10,11 @@ const SkeletonListCheckBoxes: FC<SkeletonListCheckBoxesProps> = ({ count = 5 }) 
   const sizes = ['w-40', 'w-24', 'w-32', 'w-28', 'w-36']
 
   return (
-    <ul className={s.animate}>
+    <div className={s.animate}>
       {[...new Array(count)].map((_, index) => (
-        <li className={s.checkbox} key={index}>
-          <div className={s.square} />
-          <div className={cn(s.line, sizes[index % 5])} />
-        </li>
+        <SkeletonCheckBox className={sizes[index % 5]} key={index} />
       ))}
-    </ul>
+    </div>
   )
 }
 
