ee086d272b15e355c7148e67a7c9aa3b22ef679b
fenx4 <127792143+fenx4@users.noreply.github.com>
2023-04-13 23:12:56 +0300

    Payments owner account
    add account for owner

--- backend/payments/apps/payment_accounts/serializers.py
+++ backend/payments/apps/payment_accounts/serializers.py
@@ -1,19 +1,41 @@
+from apps.external_payments.schemas import PaymentTypes
 from django.conf import settings
 from django.core.validators import MinValueValidator
+from rest_enumfield import EnumField
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Account
 
 
-class CreatePaymentSerializer(serializers.Serializer):
-    uuid = serializers.UUIDField()
-    value = serializers.DecimalField(
+
+class PaymentCommissionSerializer(serializers.Serializer):
+    payment_amount = serializers.DecimalField(
         decimal_places=2,
         max_digits=settings.MAX_BALANCE_DIGITS,
         validators=[MinValueValidator(0, message='Insufficient Funds')],
     )
-    commission = serializers.DecimalField(
-        decimal_places=1,
+    payment_type = EnumField(choices=PaymentTypes)
+
+
+class BalanceIncreaseSerializer(serializers.Serializer):
+    user_uuid = serializers.UUIDField()
+    payment_amount = serializers.DecimalField(
+        decimal_places=2,
         max_digits=settings.MAX_BALANCE_DIGITS,
         validators=[MinValueValidator(0, message='Insufficient Funds')],
     )
-    payment_type = serializers.CharField(max_length=20)
+    payment_type = EnumField(choices=PaymentTypes)
     return_url = serializers.URLField()
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['user_uuid', 'balance']
+
+
+class AccountOwnerSerializer(AccountSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta(AccountSerializer.Meta):
+        fields = AccountSerializer.Meta.fields + ['owner']
--- backend/payments/apps/payment_accounts/urls.py
+++ backend/payments/apps/payment_accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_payment/', views.CreatePaymentView.as_view()),
-    path('payment_acceptance/', views.CreatePaymentAcceptanceView.as_view()),
+    path('increase_balance/', views.BalanceIncreaseView.as_view()),
+    path('payment_commission/', views.CalculatePaymentCommissionView.as_view()),
+    path('account/owner/<uuid:user_uuid>/', views.AccountOwnerAPIView.as_view(), name='account-owner'),
 ]
