a36ad8e5075d7519355c2d3ed69736af12417262
nnnlik <makhmudov_ra@vk.com>
2023-04-15 14:42:24 +0300

    [ADD] view for displaying comments for a review

--- backend/games/community/urls.py
+++ backend/games/community/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import GameReviewCommentListView
+from .views import GameReviewListView, GameReviewCommentListView
 
 urlpatterns = [
-    path('review/<uuid:game_uuid>/', GameReviewCommentListView.as_view(), name='review'),
+    path('review/<uuid:game_uuid>/', GameReviewListView.as_view(), name='review'),
+    path('review/comments/<int:review_id>/', GameReviewCommentListView.as_view(), name='review-comments')
 ]
--- backend/games/community/views.py
+++ backend/games/community/views.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
 
-from .models import Review
-from .serializers import ReviewSerializer
+from .models import Comment, Review
+from .serializers import ReviewSerializer, CommentSerializer
 
 
-class GameReviewCommentListView(generics.ListAPIView):
+class GameReviewListView(generics.ListAPIView):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
         game_uuid = self.kwargs['game_uuid']
         return Review.objects.filter(game__id=game_uuid)
+
+
+class GameReviewCommentListView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review_id = self.kwargs['review_id']
+        return Comment.objects.filter(review__id=review_id)
