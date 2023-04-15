691da799c3670ff9ecae9827c8edb0a8227c4a86
nnnlik <makhmudov_ra@vk.com>
2023-04-15 14:41:34 +0300

    [UPDATE] update comment serializer to exclude unnecessary fields

--- backend/games/community/serializers.py
+++ backend/games/community/serializers.py
@@ -30,7 +30,6 @@ class ReactionSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    comments = serializers.SerializerMethodField()
     reactions = serializers.SerializerMethodField()
     language = serializers.StringRelatedField()
 
@@ -38,22 +37,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
 
-    def get_comments(self, obj):
-        comments = Comment.objects.filter(review=obj)
-        serializer = CommentSerializer(comments, many=True)
-        return serializer.data
-
     def get_reactions(self, obj):
         reactions = Reaction.objects.filter(review=obj)
         likes = reactions.filter(like_type='Like').count()
         dislikes = reactions.filter(like_type='Dislike').count()
         return {'like': likes, 'dislike': dislikes}
-
-
-class ReviewSerializerWithComments(ReviewSerializer):
-    comments = serializers.SerializerMethodField()
-
-    def get_comments(self, review):
-        comments = Comment.objects.filter(review=review)
-        serializer = CommentSerializer(comments, many=True)
-        return serializer.data
