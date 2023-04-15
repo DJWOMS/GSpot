018df79c9c4ebbb3085909d7d702b3eb67b4b72e
nnnlik <makhmudov_ra@vk.com>
2023-04-15 14:45:33 +0300

    [FIX] by flake8

--- backend/games/community/urls.py
+++ backend/games/community/urls.py
@@ -4,5 +4,5 @@ from .views import GameReviewListView, GameReviewCommentListView
 
 urlpatterns = [
     path('review/<uuid:game_uuid>/', GameReviewListView.as_view(), name='review'),
-    path('review/comments/<int:review_id>/', GameReviewCommentListView.as_view(), name='review-comments')
+    path('comments/<int:review_id>/', GameReviewCommentListView.as_view(), name='review-comments')
 ]
