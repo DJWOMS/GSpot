6b5b5940f2b33444ac74d851aa8d70b42345c66e
nnnlik <makhmudov_ra@vk.com>
2023-04-15 14:04:15 +0300

    [ADD] review serializer with comments and reactions

--- backend/games/community/serializers.py
+++ backend/games/community/serializers.py
@@ -1,4 +1,7 @@
+from django.db.models import Count
+
 from rest_framework import serializers
+
 from .models import Media, Social, Review, Comment, Reaction
 
 
@@ -11,22 +14,45 @@ class MediaSerializer(serializers.ModelSerializer):
 class SocialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Social
-        exclude = ('product',)
-
-
-class ReviewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ('game', 'text', 'grade')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('text',)
+        fields = '__all__'
 
 
-class LikeDislikeSerializer(serializers.ModelSerializer):
+class ReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reaction
-        fields = ('like_type',)
+        fields = ['id', 'like_type']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
+    reactions = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def get_comments(self, obj):
+        comments = Comment.objects.filter(review=obj)
+        serializer = CommentSerializer(comments, many=True)
+        return serializer.data
+
+    def get_reactions(self, obj):
+        reactions = Reaction.objects.filter(review=obj)
+        likes = reactions.filter(like_type='Like').count()
+        dislikes = reactions.filter(like_type='Dislike').count()
+        return {'like': likes, 'dislike': dislikes}
+
+
+class ReviewSerializerWithComments(ReviewSerializer):
+    comments = serializers.SerializerMethodField()
+
+    def get_comments(self, review):
+        comments = Comment.objects.filter(review=review)
+        serializer = CommentSerializer(comments, many=True)
+        return serializer.data
