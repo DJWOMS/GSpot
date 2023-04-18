414586ce98df41dcb4cee583990ea8bfc9f154a7
fenx4 <127792143+fenx4@users.noreply.github.com>
2023-04-19 01:42:22 +0300

    Update urls.py

--- backend/payments/apps/payment_accounts/urls.py
+++ backend/payments/apps/payment_accounts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('increase_balance/', views.BalanceIncreaseView.as_view()),
     path('payment_commission/', views.CalculatePaymentCommissionView.as_view()),
-    path('create_account/user/', views.UserAccountAPIView.as_view()),
+    path('create_account/', views.UserAccountAPIView.as_view()),
 ]
