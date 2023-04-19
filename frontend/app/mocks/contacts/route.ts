c02ac73afa44672ba9163e788da5955e5c1a25f2
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-04-19 17:50:25 +0300

    Frontend | fix POST request contactsPage/ ErrorMessagedd

--- frontend/app/mocks/contacts/route.ts
+++ frontend/app/mocks/contacts/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from 'next/server'
 
-//export function GET() {
-//  return NextResponse.json([...new Array(1)].map(() => generateMockContactsForm()))
-//}() => generateMockContactsForm()
 //export function POST(request: NextRequest) {
 //  console.log(request.body, 'request')
 //  return NextResponse.json(() => generateMockContactsForm())
