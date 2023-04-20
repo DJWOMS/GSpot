86e78969b2da28446b55ae3a07d1658e74509af8
tsragr <maxandreev96_2@mail.ru>
2023-04-20 13:46:13 +0300

    подключение юзеров к сокету

---
+++ backend/channels/chat/src/users/endpoints.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
+
+from chanaels_socet import manager
+
+app = FastAPI()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
