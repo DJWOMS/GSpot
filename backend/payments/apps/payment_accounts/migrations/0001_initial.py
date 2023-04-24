3a8a9b3744c59937b8b7bfb8f8163ece32a92f0d
SultanovRuslan86 <sulrus1986@gmail.com>
2023-04-24 10:38:34 +0200

    edited all files with prefix transaction to item_purchase

--- backend/payments/apps/payment_accounts/migrations/0001_initial.py
+++ backend/payments/apps/payment_accounts/migrations/0001_initial.py
@@ -2,35 +2,89 @@
 
 import apps.base.fields
 import django.core.validators
-from django.db import migrations, models
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='Account',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
                 ('user_uuid', models.UUIDField(db_index=True, editable=False, unique=True)),
-                ('balance', apps.base.fields.MoneyField(decimal_places=2, default=0, max_digits=11, validators=[django.core.validators.MinValueValidator(0, message='Insufficient Funds')])),
+                (
+                    'balance',
+                    apps.base.fields.MoneyField(
+                        decimal_places=2,
+                        default=0,
+                        max_digits=11,
+                        validators=[
+                            django.core.validators.MinValueValidator(
+                                0,
+                                message='Insufficient Funds',
+                            ),
+                        ],
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
             name='BalanceChange',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('amount', apps.base.fields.MoneyField(decimal_places=2, default=0, editable=False, max_digits=11, validators=[django.core.validators.MinValueValidator(0, message='Should be positive value')])),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
+                (
+                    'amount',
+                    apps.base.fields.MoneyField(
+                        decimal_places=2,
+                        default=0,
+                        editable=False,
+                        max_digits=11,
+                        validators=[
+                            django.core.validators.MinValueValidator(
+                                0,
+                                message='Should be positive value',
+                            ),
+                        ],
+                    ),
+                ),
                 ('date_time_creation', models.DateTimeField(auto_now_add=True, db_index=True)),
                 ('is_accepted', models.BooleanField(default=False)),
-                ('operation_type', models.CharField(choices=[('WD', 'WITHDRAW'), ('DT', 'DEPOSIT')], max_length=20)),
-                ('account_id', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='balance_changes', to='payment_accounts.account')),
+                (
+                    'operation_type',
+                    models.CharField(
+                        choices=[('WD', 'WITHDRAW'), ('DT', 'DEPOSIT')],
+                        max_length=20,
+                    ),
+                ),
+                (
+                    'account_id',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name='balance_changes',
+                        to='payment_accounts.account',
+                    ),
+                ),
             ],
             options={
                 'ordering': ['-date_time_creation'],
