36849f526c30e4af865f5f0d778784d51663cc6b
oxpaoff <1985oxpa@gmail.com>
2023-04-26 02:37:48 +0500

    Moved AUTH config to .env

--- backend/users/backend/config/settings/simple_jwt.py
+++ backend/users/backend/config/settings/simple_jwt.py
@@ -3,8 +3,8 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": os.environ["ACCESS_TOKEN_LIFETIME"],
+    "REFRESH_TOKEN_LIFETIME": os.environ["REFRESH_TOKEN_LIFETIME"],
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
