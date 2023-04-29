eee73998d97fcb9754c09f7ee01b431f6b6bcd9b
oxpaoff <1985oxpa@gmail.com>
2023-04-29 21:11:55 +0500

    TOKEN_LIFETIME from str to int

--- backend/users/backend/config/settings/jwt.py
+++ backend/users/backend/config/settings/jwt.py
@@ -4,9 +4,9 @@ from datetime import timedelta
 
 GET_TOKEN_FROM = os.environ["GET_TOKEN_FROM"]
 ACCESS_TOKEN_LIFETIME = timedelta(
-    seconds=os.environ["ACCESS_TOKEN_LIFETIME"]
+    seconds=int(os.environ["ACCESS_TOKEN_LIFETIME"])
     )
 REFRESH_TOKEN_LIFETIME = timedelta(
-    seconds=os.environ["REFRESH_TOKEN_LIFETIME"]
+    seconds=int(os.environ["REFRESH_TOKEN_LIFETIME"])
     )
 ALGORITHM = "HS256"
