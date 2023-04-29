88ddc44bf310d64d74f5d1b0471216fe05a8a308
Raphael163 <orbita.route@yandex.ru>
2023-04-29 23:11:24 +0400

    pagination

--- frontend/app/(main)/catalog/page.tsx
+++ frontend/app/(main)/catalog/page.tsx
@@ -36,7 +36,7 @@ const CatalogPage = async ({ searchParams }: { searchParams: URLSearchParams }) 
               ))}
             </div>
 
-            <Pagination />
+            <Pagination currentPage={1} totalItems={5} itemsPerPage={4} />
           </div>
         </div>
       </Section>
--- frontend/components/Pagination/Pagination.tsx
+++ frontend/components/Pagination/Pagination.tsx
@@ -1,43 +1,74 @@
 import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react'
 import Link from 'next/link'
+import PropTypes from 'prop-types'
 import s from './Pagination.module.scss'
 
-const Pagination = () => {
+interface PaginationProps {
+  currentPage: number
+  totalItems: number
+  itemsPerPage: number
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalItems, itemsPerPage }) => {
+  // рассчитываем количество страниц
+  const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+  // создаем массив страниц для отображения в пагинации
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+
   return (
     <div className="col-12">
       <div className={s.container}>
-        <div className={s.counter}>12 from 144</div>
+        <div className={s.counter}>
+          {/* отображаем текущее и общее количество элементов */}
+          {`${itemsPerPage * (currentPage - 1) + 1}-${Math.min(itemsPerPage * currentPage, totalItems)} of ${totalItems}`}
+        </div>
 
         <ul className={s.wrapper}>
+          {/* кнопка "назад" должна быть неактивной на первой странице */}
           <li className={s.item}>
-            <Link className={s.linkBtn} href="/prev">
-              <IconArrowLeft />
-            </Link>
+            {currentPage === 1 ? (
+              <IconArrowLeft className={s.disabled} />
+            ) : (
+              <Link href={`/catalog/${currentPage - 1}`} passHref>
+                <span className={s.linkBtn}>
+                  <IconArrowLeft />
+                </span>
+              </Link>
+            )}
           </li>
-          <li className={s.itemActive}>
-            <Link className={s.linkBtn} href="/1">
-              1
-            </Link>
-          </li>
+
+          {/* создаем элементы списка страниц */}
+          {pages.map((page) => (
+            <li key={page} className={`${s.item} ${currentPage === page ? s.itemActive : ''}`}>
+              <Link href={`/page/${page}`} passHref>
+                <span className={s.linkBtn}>{page}</span>
+              </Link>
+            </li>
+          ))}
+
+          {/* кнопка "вперед" должна быть неактивной на последней странице */}
           <li className={s.item}>
-            <Link className={s.linkBtn} href="/2">
-              2
-            </Link>
-          </li>
-          <li className={s.item}>
-            <Link className={s.linkBtn} href="/3">
-              3
-            </Link>
-          </li>
-          <li className={s.item}>
-            <Link className={s.linkBtn} href="/next">
-              <IconArrowRight />
-            </Link>
+            {currentPage === totalPages ? (
+              <IconArrowRight className={s.disabled} />
+            ) : (
+              <Link href={`/catalog/${currentPage + 1}`} passHref>
+                <span className={s.linkBtn}>
+                  <IconArrowRight />
+                </span>
+              </Link>
+            )}
           </li>
         </ul>
       </div>
     </div>
   )
+}
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  totalItems: PropTypes.number.isRequired,
+  itemsPerPage: PropTypes.number.isRequired,
 }
 
 export default Pagination
