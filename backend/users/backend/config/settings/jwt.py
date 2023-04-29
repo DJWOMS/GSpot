0221c089fe003cfd7f86cab191831eeef882a9d5
oxpaoff <1985oxpa@gmail.com>
2023-04-29 21:30:15 +0500

    GET_TOKEN_FROM Guard Clause

--- backend/users/backend/config/settings/jwt.py
+++ backend/users/backend/config/settings/jwt.py
@@ -1,8 +1,9 @@
 import os
+import logging
 from datetime import timedelta
 
+logger = logging.getLogger(__name__)
 
-GET_TOKEN_FROM = os.environ["GET_TOKEN_FROM"]
 ACCESS_TOKEN_LIFETIME = timedelta(
     seconds=int(os.environ["ACCESS_TOKEN_LIFETIME"])
     )
@@ -10,3 +11,12 @@ REFRESH_TOKEN_LIFETIME = timedelta(
     seconds=int(os.environ["REFRESH_TOKEN_LIFETIME"])
     )
 ALGORITHM = "HS256"
+
+GET_TOKEN_FROM = "header"
+
+if os.environ["GET_TOKEN_FROM"] not in ["header", "cookies"]:
+	logging.warning(
+		f"GET_TOKEN_FROM settled with default value {GET_TOKEN_FROM}"
+		)
+else:
+	GET_TOKEN_FROM = os.environ["GET_TOKEN_FROM"]
