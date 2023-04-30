a1dc38ba5ba529d44fb5b5c812133243daf170dd
oxpaoff <1985oxpa@gmail.com>
2023-04-30 14:15:45 +0500

    Removed SimpleJWT default urls

--- backend/users/backend/config/urls.py
+++ backend/users/backend/config/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 
 from .yasg import urlpatterns as yasg_doc
-from common.services.jwt.urls import urlpatterns as jwt_url
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	
 ]
+
 urlpatterns += yasg_doc
-urlpatterns += jwt_url
