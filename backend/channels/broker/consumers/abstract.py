85e5e3df28ab186b5f412bacfde6010480ea406d
Kiel Ed <68655454+kieled@users.noreply.github.com>
2023-04-30 23:22:02 +0300

    Channels | added linters and pre-commit, fixed some docker errors (#189)
    * added temp ci/cd, fixed some docker errors
    
    * fixed requirements

--- backend/channels/broker/consumers/abstract.py
+++ backend/channels/broker/consumers/abstract.py
@@ -3,7 +3,6 @@ from abc import ABCMeta, abstractmethod
 
 from aio_pika.message import IncomingMessage
 from aio_pika.queue import Queue
-import os
 
 
 async def mark_message_processed(orig_message: IncomingMessage):
@@ -16,15 +15,18 @@ async def mark_message_processed(orig_message: IncomingMessage):
 
 
 class RabbitMQConsumer(metaclass=ABCMeta):
-    """RabbitMQ consumer abstract class responsible for consuming data from the queue."""
+    """
+    RabbitMQ consumer abstract class responsible
+    for consuming data from the queue.
+    """
 
     def __init__(
-        self,
-        queue: Queue,
-        iterator_timeout: int = 5,
-        iterator_timeout_sleep: float = 5.0,
-        *args,
-        **kwargs,
+            self,
+            queue: Queue,
+            iterator_timeout: int = 5,
+            iterator_timeout_sleep: float = 5.0,
+            *args,
+            **kwargs,
     ):
         self.queue = queue
         self.iterator_timeout = iterator_timeout
@@ -32,7 +34,10 @@ class RabbitMQConsumer(metaclass=ABCMeta):
         self.consuming_flag = True
 
     async def consume(self):
-        """Consumes data from RabbitMQ queue forever until `stop_consuming()` is called."""
+        """
+        Consumes data from RabbitMQ queue forever
+        until `stop_consuming()` is called.
+        """
         async with self.queue.iterator(timeout=self.iterator_timeout) as queue_iterator:
             while self.consuming_flag:
                 try:
