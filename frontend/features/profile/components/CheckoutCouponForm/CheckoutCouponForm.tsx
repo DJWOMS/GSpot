52275d601dff7e6e66585b5c53cd8f232e0ff7eb
Aleksandr <101511388+smnov@users.noreply.github.com>
2023-05-01 14:27:40 +0800

    Frontend | CheckoutCouponForm & CheckoutForm components added (#158)
    * Frontend | CheckoutCouponForm & CheckoutForm components added
    
    * Frontend | Checkout style fixes
    
    * small html and style fixes
    
    * Style fixes
    
    * test

---
+++ frontend/features/profile/components/CheckoutCouponForm/CheckoutCouponForm.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+import { IconX } from '@tabler/icons-react'
+import { Input } from 'components/Form'
+import { fetchServerSide } from 'lib/fetchServerSide'
+import s from './CheckoutCouponForm.module.scss'
+
+const CheckoutCouponForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm()
+  const [coupon, setCoupon] = useState(false)
+  const [couponInvalid, setCouponInvalid] = useState(false)
+
+  const onSubmitCoupon = async (data: any) => {
+    const response = await fetchServerSide({
+      path: '/profile/checkout/coupon',
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+    if (response === true) {
+      setCoupon(true)
+    } else if (response === false) {
+      setCouponInvalid(true)
+    }
+  }
+
+  return (
+    <>
+      <form className={s.form} onSubmit={handleSubmit(onSubmitCoupon)}>
+        <Controller
+          control={control}
+          name="coupon"
+          defaultValue=""
+          render={({ field }) => {
+            return <Input {...field} placeholder="Coupon code" />
+          }}
+        />
+        <ErrorMessage errors={errors} name="couponError" render={({ message }) => <p>{message}</p>} />
+        <button type="submit" className={s.formBtn} disabled={coupon}>
+          Применить
+        </button>
+        {coupon && (
+          <div className="inline-flex">
+            <p className={s.coupon}>Купон активирован</p>
+            <button className={s.deleteCoupon} type="button" onClick={() => setCoupon(false)}>
+              <IconX />
+            </button>
+          </div>
+        )}
+        {couponInvalid && <p className={s.couponInvalid}>Некорректный купон</p>}
+      </form>
+    </>
+  )
+}
+
+export { CheckoutCouponForm }
