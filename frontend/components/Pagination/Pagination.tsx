cd13b92a5a1e0a6ed004ba41e1c3c00568fa7e4a
Raphael163 <orbita.route@yandex.ru>
2023-05-02 16:33:45 +0400

    pagination style rebuild

--- frontend/components/Pagination/Pagination.tsx
+++ frontend/components/Pagination/Pagination.tsx
@@ -9,15 +9,25 @@ const Pagination = () => {
   return (
     <ReactPaginate
       // css settings
-      containerClassName={style.root}
-      breakLabel="..."
-      nextLabel={<IconArrowRight size={20} />}
+      containerClassName={style.container}
+      pageClassName={style.item}
+      activeClassName={style.itemActive}
+      breakClassName={style.item}
+      previousClassName={style.linkBtn}
+      nextClassName={style.linkBtn}
       previousLabel={<IconArrowLeft size={20} />}
+      nextLabel={<IconArrowRight size={20} />}
       // settings ReactPaginate
       onPageChange={(event) => console.log(event)}
       pageRangeDisplayed={5}
-      pageCount={5}
+      pageCount={100}
       renderOnZeroPageCount={null}
+      // additional css settings
+      pageLinkClassName={style.linkBtn}
+      previousLinkClassName={style.linkBtn}
+      nextLinkClassName={style.linkBtn}
+      activeLinkClassName={style.activeLink}
+      breakLinkClassName={style.linkBtn}
     />
   )
 }
