1a18181ffd1f3c03969ec998841b5e182a45ae17
Roman-Zhirovskis <tesslogun@gmail.com>
2023-05-04 18:45:25 +0300

    Users| Added CRUD functionality for Country and Content Type models

--- backend/users/backend/config/urls.py
+++ backend/users/backend/config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from .yasg import urlpatterns as yasg_doc
-
+from common.urls import urlpatterns as common_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += yasg_doc
+urlpatterns += common_urls
