7cd0efb8aeb023dc17e2bc7bda9cfcd68697213a
mozambique_ya <alexandrzvar@yandex.ru>
2023-05-04 10:46:11 +0300

    remove admin(default django) urls

--- backend/users/backend/config/urls.py
+++ backend/users/backend/config/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 
 from .yasg import urlpatterns as yasg_doc
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
 ]
 
 urlpatterns += yasg_doc
