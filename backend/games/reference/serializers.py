b4056de04feb880793623e754fc238bb270244b0
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-05 13:24:44 +0300

    [FIX] sub genre serailizer

--- backend/games/reference/serializers.py
+++ backend/games/reference/serializers.py
@@ -9,13 +9,11 @@ class LanguageSerializer(serializers.ModelSerializer):
 
 
 class ProductLanguageSerializer(serializers.ModelSerializer):
-    language_name = serializers.CharField(source='language.name')
-
     class Meta:
         model = ProductLanguage
         fields = (
             'id',
-            'language_name',
+            'language',
             'interface',
             'subtitles',
             'voice'
@@ -32,18 +30,6 @@ class SubGenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubGenre
         fields = ('id', 'name')
-
-    def to_representation(self, instance):
-        # Get subgenres from context
-        subgenres = self.context.get('subgenres', [])
-
-        # Filter subgenres that belong to the current instance
-        subgenres = [subgenre for subgenre in subgenres if subgenre.genre_id == instance.genre_id]
-
-        # Serialize subgenres and add them to the representation
-        data = super().to_representation(instance)
-        data['subgenres'] = SubGenreSerializer(subgenres, many=True).data
-        return data
 
 
 class GenreGamesSerializer(serializers.ModelSerializer):
