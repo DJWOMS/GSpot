a9f2b9158c9f8db012665b1e4208e732e6c0b745
BogdanVol <89146515+BogdanVol@users.noreply.github.com>
2023-05-07 20:20:56 +0300

    Games | create filters for games (#197)
    filters for games

--- backend/games/core/urls.py
+++ backend/games/core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GameViewSet, DlcViewSet, SystemRequirementViewSet
+from .views import GameViewSet, DlcViewSet, SystemRequirementViewSet, ProductViewSet
 
 api_router = DefaultRouter()
 api_router.register('games', GameViewSet, basename='games')
 api_router.register('dlc', DlcViewSet, basename='dlc')
 api_router.register('system_requirement', SystemRequirementViewSet, basename='system_requirement')
+api_router.register('products', ProductViewSet, basename='products')
 
 
 urlpatterns = [
