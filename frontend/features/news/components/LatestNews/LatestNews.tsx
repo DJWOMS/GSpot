16339b49540d7228cad9deb10ae3db7d7a96452b
Kiel Ed <kieledssh@gmail.com>
2023-05-07 17:30:40 +0300

    small fixes

--- frontend/features/news/components/LatestNews/LatestNews.tsx
+++ frontend/features/news/components/LatestNews/LatestNews.tsx
@@ -66,44 +66,6 @@ const LatestNews = () => {
           />
         </div>
       </div>
-
-      {/* <div className="flex gap-4">
-        <div className="w-full">
-          <NewsItem
-            title={'New hot race from your favorite computer games studio'}
-            imageSrc={'https://loremflickr.com/640/400'}
-            date={'3 h ago'}
-            category={'Overview'}
-            size={'normal'}
-            commentsCount={2}
-            url={'#'}
-          />
-        </div>
-
-        <div className="w-full">
-          <NewsItem
-            title={'New hot race from your favorite computer games studio'}
-            imageSrc={'https://loremflickr.com/640/400'}
-            date={'3 h ago'}
-            category={'PC'}
-            size={'normal'}
-            commentsCount={0}
-            url={'#'}
-          />
-        </div>
-
-        <div className="w-full">
-          <NewsItem
-            title={'New hot race from your favorite computer games studio'}
-            imageSrc={'https://loremflickr.com/640/400'}
-            date={'3 h ago'}
-            category={'VR'}
-            size={'normal'}
-            commentsCount={50}
-            url={'#'}
-          />
-        </div>
-      </div> */}
     </Section>
   )
 }
