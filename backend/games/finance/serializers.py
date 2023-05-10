9a2debeef9e1487150fc67013445199f9765aa27
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-05-10 19:43:31 +0700

    create offer

---
+++ backend/games/finance/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from core.models.product import Product
+from finance.models.offer import Price, ProductOffer
+from finance.models import Offer
+from django.db import transaction
+
+
+class PriceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Price
+        fields = '__all__'
+
+
+class OfferSerializer(serializers.ModelSerializer):
+    price = PriceSerializer()
+    products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+
+    class Meta:
+        model = Offer
+        fields = ('id', 'created_by', 'is_active', 'products', 'price')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        products_data = validated_data.pop('products')
+        developer = set()
+        for product in products_data:
+            developer.add(product.developers_uuid)
+            if len(developer) > 1:
+                raise serializers.ValidationError(
+                    {"mesage": 'Нельзя создать пакет с разными разработчиками'}
+                )
+        price_date = validated_data.pop('price')
+        price = Price.objects.create(**price_date)
+        offer = Offer.objects.create(**validated_data, price=price)
+        product_offer = [
+            ProductOffer(
+                offer=offer,
+                product=product,
+                created_by=product.developers_uuid
+            ) for product in products_data
+        ]
+        ProductOffer.objects.bulk_create(product_offer)
+        return offer
