4b0e1413beba654a9efc435b7bfd8da110c6df24
nikitqa48 <Princen1k@yandex.com>
2023-05-11 00:05:50 +0300

    add model schema for notification

--- backend/channels/broker/consumers/notifications/consumer.py
+++ backend/channels/broker/consumers/notifications/consumer.py
@@ -19,7 +19,6 @@ class NotificationConsumer(RabbitMQConsumer):
         # logger.info(orig_message.body)
 
     async def insert_message(self, message):
-        print(message)
         db = self.db_client.GSpot
         collection = db.users
         document = {'key': 'value'}
