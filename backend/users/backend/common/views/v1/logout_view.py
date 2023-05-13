bf9343ebbff45680eb2d659f451ea3e2d1412b3f
oxpaoff <1985oxpa@gmail.com>
2023-05-13 18:16:33 +0500

    Changed APIView to GenericApiView

--- backend/users/backend/common/views/v1/logout_view.py
+++ backend/users/backend/common/views/v1/logout_view.py
@@ -1,12 +1,12 @@
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from common.services.jwt.blacklist import TokenBlackList
 
 
-class JWTLogoutView(APIView):
+class JWTLogoutView(GenericAPIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
