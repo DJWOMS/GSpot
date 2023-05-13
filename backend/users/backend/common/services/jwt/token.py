80d957e29170c3757205f93fb17648da744d505f
Балаби <turembekov@vk.com>
2023-05-13 10:17:31 +0000

    USERS | refactoring class Token

--- backend/users/backend/common/services/jwt/token.py
+++ backend/users/backend/common/services/jwt/token.py
@@ -10,7 +10,7 @@ from common.services.jwt.mixins import JWTMixin
 
 class Token(BaseToken, JWTMixin):
     def generate_access_token(self, data: dict = {}) -> str:
-        self.check_data_for_payload(data)
+        self._validate_payload_data(data)
         iat = timezone.localtime()
         exp = iat + settings.ACCESS_TOKEN_LIFETIME
         payload = {
@@ -22,34 +22,36 @@ class Token(BaseToken, JWTMixin):
         access_token = self._encode(payload)
         return access_token
 
-    @staticmethod
-    def check_data_for_payload(data: dict):
-        required = ['user_id', 'role']
-        for key in required:
-            if key not in data:
-                raise PayloadError(f"Payload must contain - {key}")
+    def _validate_payload_data(self, data: dict) -> None:
+        required_fields = ['user_id', 'role']
+        for field in required_fields:
+            if field not in data:
+                raise PayloadError(f"Payload must contain - {field}")
 
-    def generate_refresh_token(self) -> str:
+    def generate_refresh_token(self, user_id: str, role: str) -> str:
         iat = timezone.localtime()
         exp = iat + settings.REFRESH_TOKEN_LIFETIME
         payload = {
             "token_type": "refresh",
             "iat": int(iat.timestamp()),
             "exp": int(exp.timestamp()),
+            "user_id": user_id,
+            "role": role,
         }
         refresh_token = self._encode(payload)
         return refresh_token
 
-    def generate_tokens(self) -> dict:
-        tokens = {"access": self.generate_access_token(), "refresh": self.generate_refresh_token()}
-        return tokens
+    def generate_tokens(self, data: dict) -> dict:
+        access_token = self.generate_access_token(data)
+        refresh_token = self.generate_refresh_token(data.id, data.role)
+        return {"access": access_token, "refresh": refresh_token}
 
-    def check_token(self, token: str):
-        self.check_exp(token)
-        self.check_signature(token)
+    def check_token(self, token: str) -> bool:
+        self._check_exp(token)
+        self._check_signature(token)
         return True
 
-    def check_exp(self, token: str) -> int:
+    def _check_exp(self, token: str) -> int:
         exp_left = self.check_exp_left(token)
         if exp_left == 0:
             raise TokenExpired()
@@ -66,5 +68,5 @@ class Token(BaseToken, JWTMixin):
         else:
             return 0
 
-    def check_signature(self, token: str):
+    def _check_signature(self, token: str) -> None:
         self._decode(token)
