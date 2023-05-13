3111c67ebece623623268ca2f5b485e77b03b5a3
oxpaoff <1985oxpa@gmail.com>
2023-05-13 08:43:51 +0500

    Added validating refresh_token

--- backend/users/backend/common/services/jwt/blacklist.py
+++ backend/users/backend/common/services/jwt/blacklist.py
@@ -1,6 +1,10 @@
+from common.services.jwt.token import Token
+
+
 class TokenBlackList:
     def __init__(self, token: str):
         self.token = token
 
     def add(self):
+        Token().check_token(self.token)
         pass
