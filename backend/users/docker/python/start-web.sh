ef672f110b682d9bcd880712e33c071544cf4bcd
oxpaoff <1985oxpa@gmail.com>
2023-05-15 00:56:43 +0500

    Edited start-web.sh

--- backend/users/docker/python/start-web.sh
+++ backend/users/docker/python/start-web.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
@@ -10,6 +10,6 @@ if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
     --log-level ${DJANGO_LOG_LEVEL,,} \
     --access-logfile - config.wsgi:application
 else
-  python manage.py initadmin
+#   python manage.py initadmin
   python manage.py runserver 0.0.0.0:8000
 fi
