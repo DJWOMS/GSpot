ab5c5c20a924c00f032137dffcfa3c5bb43064e7
nikitqa48 <54353932+nikitqa48@users.noreply.github.com>
2023-05-15 05:08:57 +0300

    Channels || fix conflicts (#241)
    * start channels
    
    * add readme
    
    * refactor readme
    
    * remove init file
    
    * fix poetry, fix requirements for chat
    
    ---------
    
    Co-authored-by: nikitqa48 <Princen1k@yandex.com>

--- backend/channels/chat/main.py
+++ backend/channels/chat/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from src.router import router
+from services.database import db
+
 
 app = FastAPI()
 
 app.include_router(router)
 
+
+@app.on_event('startup')
+async def init_db():
+    db.session = await db.client.start_session()
