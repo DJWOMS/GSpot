72afe9f55340e1a522f8b7a2d06b62870dbd522f
nikitqa48 <54353932+nikitqa48@users.noreply.github.com>
2023-05-16 22:59:06 +0300

    Channels || websocket routing (#246)
    * rename tire service to databus
    
    * add websocket routing
    
    * remove services files
    
    ---------
    
    Co-authored-by: nikitqa48 <Princen1k@yandex.com>

--- backend/channels/chat/core/redis.py
+++ backend/channels/chat/core/redis.py
@@ -1,10 +1,10 @@
 from config.redis import redis_config
 from redis.asyncio.client import PubSub, Redis
-import redis.asyncio as redis
+import redis.asyncio as rd
 
 
 class RedisManager:
-    redis: Redis = redis.from_url(
+    redis: Redis = rd.from_url(
         redis_config.url,
         encoding='utf-8',
         decode_responses=True
