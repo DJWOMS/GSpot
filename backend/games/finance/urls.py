1507d77559a56e5127ff2e06b4789886a8c78b95
KeirisTorn <Keiristorn@gmail.com>
2023-05-20 13:13:58 +0300

    create endpoint for Library

--- backend/games/finance/urls.py
+++ backend/games/finance/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import OfferAPIView
+from finance.views import OfferAPIView, ShowLibraryView
 
 
 urlpatterns = [
     path('offer/', OfferAPIView.as_view(), name="offer"),
+    path("library/<user>", ShowLibraryView.as_view(),
+         name="all_products_in_library")
 ]
