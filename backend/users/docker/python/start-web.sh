76bf1f4dccffaa151c876f17b08b3c0a3f6345a5
oxpaoff <1985oxpa@gmail.com>
2023-05-22 00:06:00 +0500

    Removed DJANGO_ENV condition in start-web.sh

--- backend/users/docker/python/start-web.sh
+++ backend/users/docker/python/start-web.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 python manage.py migrate
-if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
-  gunicorn -b 0.0.0.0:8000 \
-    --log-level ${DJANGO_LOG_LEVEL,,} \
-    --access-logfile - config.wsgi:application
-else
-  python manage.py runserver 0.0.0.0:8000
-fi
+
+gunicorn -b 0.0.0.0:8000 \
+--log-level $DJANGO_LOG_LEVEL \
+--access-logfile - config.wsgi:application
