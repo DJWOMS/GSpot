caa27989de9ad803085b65c58d4cd92eb14dc2d4
JulieBenchuk <juliebenchuk@gmail.com>
2023-05-23 00:59:42 +0300

    frontend: changed pagination to own implementation, some css changes

--- frontend/components/Pagination/Pagination.tsx
+++ frontend/components/Pagination/Pagination.tsx
@@ -1,36 +1,63 @@
-'use client'
-
-import React from 'react'
-import ReactPaginate from 'react-paginate'
+import React, { FC, useMemo } from 'react'
 import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react'
+import Link from 'next/link'
 import s from './Pagination.module.css'
 
-const Pagination = () => {
+interface Props {
+  path: string
+  currentPage: number
+  query?: { [key: string]: string | string[] | undefined }
+}
+const Pagination: FC<Props> = ({ path, currentPage, query }) => {
+  const pageCount = 5
+  const visiblePageButtonsCount = 3
+  const visiblePages = useMemo(() => {
+    const halfCount = Math.floor(visiblePageButtonsCount / 2)
+    let startPage = currentPage <= halfCount ? 1 : currentPage - halfCount
+    const endPage = Math.min(startPage + visiblePageButtonsCount - 1, pageCount)
+    if (endPage === pageCount) {
+      startPage = Math.max(pageCount - visiblePageButtonsCount + 1, 1)
+    }
+    return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i)
+  }, [currentPage, pageCount, visiblePageButtonsCount])
+
   return (
     <div className={s.wrapper}>
-      <div className={s.counter}>12 from 3</div>
-      <ReactPaginate
-        // css settings
-        containerClassName={s.container}
-        pageClassName={s.item}
-        activeClassName={s.itemActive}
-        breakClassName={s.item}
-        previousClassName={s.linkBtn}
-        nextClassName={s.linkBtn}
-        previousLabel={<IconArrowLeft size={17} />}
-        nextLabel={<IconArrowRight size={17} />}
-        // settings ReactPaginate
-        onPageChange={console.log}
-        pageRangeDisplayed={5}
-        pageCount={3}
-        renderOnZeroPageCount={null}
-        // additional css settings
-        pageLinkClassName={s.linkBtn}
-        previousLinkClassName={s.linkBtn}
-        nextLinkClassName={s.linkBtn}
-        activeLinkClassName={s.activeLink}
-        breakLinkClassName={s.linkBtn}
-      />
+      <div className={s.counter}>{`${visiblePages.length * currentPage} from ${
+        visiblePages.length * pageCount
+      }`}</div>
+      <ul className={s.container}>
+        {currentPage > 1 && (
+          <li className={s.linkBtn}>
+            <Link className={s.linkBtn} href={{ pathname: path, query: { page: currentPage - 1 } }}>
+              {<IconArrowLeft size={17} />}
+            </Link>
+          </li>
+        )}
+        {visiblePages.map((pageNumber) => (
+          <li key={pageNumber} className={`${s.item} ${currentPage === pageNumber ? s.itemActive : ''}`}>
+            <Link
+              href={{ pathname: path, query: query ? { ...query, page: pageNumber } : { page: pageNumber } }}
+              className={s.linkBtn}
+            >
+              {pageNumber}
+            </Link>
+          </li>
+        ))}
+        {currentPage < pageCount && (
+          <li className={s.linkBtn}>
+            <Link
+              className={s.linkBtn}
+              href={{
+                pathname: path,
+                query: { page: currentPage + 1 },
+              }}
+            >
+              {<IconArrowRight size={17} />}
+            </Link>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
