85dd85faa91753ed661804d43687b63d6c93afc5
Roman-Zhirovskis <tesslogun@gmail.com>
2023-05-23 11:47:22 +0300

    Users| Added CRUD functionality for Country and Content Type models
    *Added base_serializers
    *Added CRUD for Country and Content Type models

---
+++ backend/users/backend/base/views/base_views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from common.permissons import IsAdminSuperUser
+
+
+class BaseAdminSuperUserViewSet(viewsets.ModelViewSet):
+    """Base class, where list, retrieve have the permissions - IsAuthenticated,
+    other methods have the permission - IsAdminSuperUser"""
+
+    queryset = None
+    serializer_class = None
+    permission_classes = [IsAdminSuperUser]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
