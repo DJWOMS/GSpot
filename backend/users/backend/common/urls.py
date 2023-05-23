03c8ff01cc4d6a463121f8068837bc5869999ec9
Roman-Zhirovskis <tesslogun@gmail.com>
2023-05-23 11:47:22 +0300

    Users| Added CRUD functionality for Country and Content Type models
    *Added base_serializers
    *Added CRUD for Country and Content Type models

--- backend/users/backend/common/urls.py
+++ backend/users/backend/common/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from common.views.v1.views import CountryViewSet, ContactTypeViewSet
 from common.views.v1.logout_view import JWTLogoutView
 
-urlpatterns = [path('logout/', JWTLogoutView.as_view(), name="logout")]
+
+router = routers.DefaultRouter()
+router.register(r"countries", CountryViewSet, basename="country")
+router.register(r"contact_types", ContactTypeViewSet, basename="contact_type")
+
+urlpatterns = [
+    path("logout/", JWTLogoutView.as_view(), name="logout"),
+    path("common/", include(router.urls)),
+]
