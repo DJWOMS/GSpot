dfe5450e9d7d2bf9013ae1aec28950cf3ddc86cf
tsragr <maxandreev96_2@mail.ru>
2023-05-25 16:06:37 +0300

    http/ws endpoints for notifications

--- backend/channels/chat/src/notifications/repository.py
+++ backend/channels/chat/src/notifications/repository.py
@@ -10,17 +10,17 @@ class NotificationRepo:
 
     @staticmethod
     async def get_by_id(_id: str):
-        return await db.your_mongodb_db.get_collection('notification').find_one({'_id': _id})
+        return await db.client.your_mongodb_db.get_collection('notification').find_one({'_id': _id})
 
     @staticmethod
     async def get_user_notification(user_id: str):
         _notifications = []
-        collection = db.your_mongodb_db.get_collection('notification').find({'user_id': user_id})
+        collection = db.client.your_mongodb_db.get_collection('notification').find({'user_id': user_id})
         async for notification in collection:
             _notifications.append(notification)
         return _notifications
 
     @staticmethod
     async def update_status_notification(_id: str, status: str):
-        await db.your_mongodb_db.get_collection('notification').update_one({'_id': _id}, {'$set': {'status': status}})
+        await db.client.your_mongodb_db.get_collection('notification').update_one({'_id': _id}, {'$set': {'status': status}})
         return NotificationRepo.get_by_id(_id)
