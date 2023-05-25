a0a7aaa58b8448cf03c2a683aeefe08e627914a1
KeirisTorn <Keiristorn@gmail.com>
2023-05-25 23:53:10 +0300

    corrected remarks

--- backend/games/finance/urls.py
+++ backend/games/finance/urls.py
@@ -4,6 +4,5 @@ from finance.views import OfferAPIView, ShowLibraryView
 
 urlpatterns = [
     path('offer/', OfferAPIView.as_view(), name="offer"),
-    path("library/<user>", ShowLibraryView.as_view(),
-         name="all_products_in_library")
+    path("library/<user>", ShowLibraryView.as_view(), name="all_products_in_library")
 ]
