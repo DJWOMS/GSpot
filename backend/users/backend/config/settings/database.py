16ac7a906b56d990f73f52175c77c59fab7c9a12
oxpaoff <1985oxpa@gmail.com>
2023-05-28 19:22:34 +0500

    renamed database variables names

--- backend/users/backend/config/settings/database.py
+++ backend/users/backend/config/settings/database.py
@@ -7,7 +7,7 @@ DATABASES = {
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["DB_HOST"],
-        "PORT": os.environ["DB_PORT"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
