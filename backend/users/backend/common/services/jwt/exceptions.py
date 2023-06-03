c2ec96c335de1e337cf8042abfac590f87b3f2dd
Viktor <75262818+oxpaoff@users.noreply.github.com>
2023-06-03 14:21:42 +0500

    Changed Exception to ValidationError (#285)

--- backend/users/backend/common/services/jwt/exceptions.py
+++ backend/users/backend/common/services/jwt/exceptions.py
@@ -1,10 +1,13 @@
-class TokenExpired(Exception):
+from django.core.exceptions import ValidationError
+
+
+class TokenExpired(ValidationError):
     pass
 
 
-class TokenInvalid(Exception):
+class TokenInvalid(ValidationError):
     pass
 
 
-class PayloadError(Exception):
+class PayloadError(ValidationError):
     pass
