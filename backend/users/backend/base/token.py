e2bf39fc03e40f8bf857fdeb61d60bad38ca2cb8
oxpaoff <1985oxpa@gmail.com>
2023-06-04 22:51:44 +0500

    Some edits in BaseToken

--- backend/users/backend/base/token.py
+++ backend/users/backend/base/token.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 
 class BaseToken(ABC):
     @abstractmethod
-    def generate_access_token():
+    def generate_access_token(self, data: dict):
         pass
 
     @abstractmethod
-    def generate_refresh_token():
+    def generate_refresh_token(self, data: dict):
         pass
 
     @abstractmethod
-    def check_token():
+    def check_token(self, token: str):
         pass
