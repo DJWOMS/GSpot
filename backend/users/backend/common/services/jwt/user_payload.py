989747afad7f6c2f6bfbb82750a5e502245d614c
oxpaoff <1985oxpa@gmail.com>
2023-06-09 01:10:13 +0500

    Added user types payloads

--- backend/users/backend/common/services/jwt/user_payload.py
+++ backend/users/backend/common/services/jwt/user_payload.py
@@ -1,4 +1,8 @@
+from administrator.models import Admin
 from base.models import BaseAbstractUser
+from base.payload import BasePayload
+from customer.models import CustomerUser
+from developer.models import CompanyUser
 
 
 class CommonUserPayload:
@@ -15,12 +19,44 @@ class CommonUserPayload:
         return common_payload
 
 
+class AdminPayload(BasePayload, CommonUserPayload):
+    def __init__(self, user: BaseAbstractUser):
+        self.user = user
+
+    def generate_payload(self):
+        common_payload = self.get_common_fields()
+        return common_payload
+
+
+class DeveloperPayload(BasePayload, CommonUserPayload):
+    def __init__(self, user: BaseAbstractUser):
+        self.user = user
+
+    def generate_payload(self):
+        common_payload = self.get_common_fields()
+        return common_payload
+
+
+class CustomerPayload(BasePayload, CommonUserPayload):
+    def __init__(self, user: BaseAbstractUser):
+        self.user = user
+
+    def generate_payload(self):
+        common_payload = self.get_common_fields()
+        common_payload['age'] = self.user.age
+        return common_payload
+
+
 class PayloadFactory:
-    payload_types = {}
+    payload_types = {
+        Admin: AdminPayload,
+        CompanyUser: DeveloperPayload,
+        CustomerUser: CustomerPayload,
+    }
 
     def create_payload(self, user_type: BaseAbstractUser) -> dict:
         payload_class = self.payload_types.get(user_type)
         if payload_class:
-            return payload_class.generate_payload()
+            return payload_class(user_type).generate_payload()
         else:
             raise ValueError("Unknown user type.")
