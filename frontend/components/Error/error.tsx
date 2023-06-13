5d2a5c80668cccd88c7cc26f55b4e6dcfe0b3551
arolina sepenok <karolina@inno.menu>
2023-06-13 21:36:13 +0300

    Frontend|fix_contacts_validation

--- frontend/components/Error/error.tsx
+++ frontend/components/Error/error.tsx
@@ -2,24 +2,12 @@
 
 import s from './error.module.css'
 
-export default function Error({
-  error,
-  reset,
-  statusCode,
-  textError,
-}: {
-  error: Error
-  statusCode: string
-  reset: () => void
-  textError: string
-}) {
+export default function Error({ reset, textError }: { reset: () => void; textError: string }) {
   const onClickHandler = () => reset()
 
   return (
     <div className={s.errorsContainer}>
-      {statusCode && <h5 className={s.statusCode}> {statusCode}</h5>}
       <p className={s.title}> {textError}</p>
-      <p className={s.errorMessage}>{error.message}</p>
       <button className={s.btnError} onClick={onClickHandler}>
         Пробовать снова
       </button>
