a701fc7750188a5fc06579ab7a48307c010dde52
kindevelopment <madeinbosco@inbox.ru>
2023-06-13 20:18:23 +0300

    Added functions for Additions, Acceptance, Deviations, Deletions request friend

---
+++ backend/users/backend/customer/services/check_friendship_user.py
@@ -0,0 +1,50 @@
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from customer.models import FriendShipRequest
+
+
+class RequestFriends:
+    @staticmethod
+    def check_send_request(request_user, user_add):
+        if FriendShipRequest.objects.filter(
+            Q(sender=request_user, receiver=user_add, status__in=['REQUESTED'])
+            | Q(sender=user_add, receiver=request_user, status__in=['REQUESTED'])
+        ).exists():
+            raise ValidationError(message='A friend request has already been sent to this user')
+
+        elif FriendShipRequest.objects.filter(
+            Q(sender=request_user, receiver=user_add, status__in=['ACCEPTED'])
+            | Q(sender=user_add, receiver=request_user, status__in=['ACCEPTED'])
+        ).exists():
+            raise ValidationError(message='You are already friends')
+
+        elif FriendShipRequest.objects.filter(
+            Q(sender=request_user, receiver=user_add, status__in=['REJECTED'])
+            | Q(sender=user_add, receiver=request_user, status__in=['REJECTED'])
+        ).exists():
+            raise ValidationError(message='The user rejected your friend request')
+
+    @staticmethod
+    def check_accept_request(request_user, user_add):
+        if not FriendShipRequest.objects.filter(
+            sender=user_add, receiver=request_user, status__in=['REQUESTED']
+        ).exists():
+            raise ValidationError(message='This user did not send the request')
+
+        elif FriendShipRequest.objects.filter(
+            sender=user_add, receiver=request_user, status__in=['REJECTED']
+        ).exists():
+            raise ValidationError(message='The user rejected your request earlier')
+
+        elif FriendShipRequest.objects.filter(
+            sender=user_add, receiver=request_user, status__in=['ACCEPTED']
+        ).exists():
+            raise ValidationError(message='You are already friends')
+
+    @staticmethod
+    def check_delete_friend(request_user, user_add):
+        if not FriendShipRequest.objects.filter(
+            Q(sender=user_add, receiver=request_user, status__in=['ACCEPTED'])
+            | Q(sender=request_user, receiver=user_add, status__in=['ACCEPTED'])
+        ).exists():
+            raise ValidationError(message='This user is not in friends')
