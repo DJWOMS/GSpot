64c7507b4fa1685e86bcd9f0fca872fddeb3db22
gravity <gravity4849@gmail.com>
2023-06-14 10:03:16 +0300

    docker-compose fix

--- backend/users/backend/manage.py
+++ backend/users/backend/manage.py
@@ -2,12 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../.env"))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
