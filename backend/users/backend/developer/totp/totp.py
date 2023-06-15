d0e7828abd6bfa6bd532e7106ed101b06d0b695d
oxpaoff <1985oxpa@gmail.com>
2023-06-15 22:45:13 +0500

    added method for generating totp

--- backend/users/backend/developer/totp/totp.py
+++ backend/users/backend/developer/totp/totp.py
@@ -1,5 +1,10 @@
+import uuid
+
 from base.tokens.totp import BaseTOTPToken
 
 
 class TOTPToken(BaseTOTPToken):
-    pass
+    @staticmethod
+    def generate_totp() -> str:
+        totp = str(uuid.uuid4())
+        return totp
