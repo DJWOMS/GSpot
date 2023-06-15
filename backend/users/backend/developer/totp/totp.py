e00be402505665e160716742a3f0b89e496e5eb6
oxpaoff <1985oxpa@gmail.com>
2023-06-15 23:11:08 +0500

    added method for putting totp to redis

--- backend/users/backend/developer/totp/totp.py
+++ backend/users/backend/developer/totp/totp.py
@@ -1,6 +1,8 @@
 import uuid
 
+from base.models import BaseAbstractUser
 from base.tokens.totp import BaseTOTPToken
+from utils.db.redis_client import RedisTotpClient
 
 
 class TOTPToken(BaseTOTPToken):
@@ -8,3 +10,11 @@ class TOTPToken(BaseTOTPToken):
     def generate_totp() -> str:
         totp = str(uuid.uuid4())
         return totp
+
+    def add_to_redis(self, totp: str, user: BaseAbstractUser):
+        redis_client = RedisTotpClient()
+        value = {
+            "user_id": str(self.user.id),
+            "role": user._meta.app_label,
+        }
+        redis_client.add_token(token=totp, value=value)
