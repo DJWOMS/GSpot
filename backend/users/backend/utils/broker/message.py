d00820c06a58c2da79a2ceb0bd3141ff9456f3ef
oxpaoff <1985oxpa@gmail.com>
2023-06-16 00:36:27 +0500

    Added init=False to dataclasses

--- backend/users/backend/utils/broker/message.py
+++ backend/users/backend/utils/broker/message.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from typing import Any
 
 
-@dataclass
+@dataclass(init=False)
 class BaseMessage:
     exchange_name: str
     routing_key: str
@@ -14,37 +14,37 @@ class BaseMessage:
         return message_json.encode()
 
 
-@dataclass
+@dataclass(init=False)
 class DevActivationMessage(BaseMessage):
     exchange_name = 'dev_activation_exchange'
     routing_key = 'dev_activation_queue'
 
 
-@dataclass
+@dataclass(init=False)
 class ClientActivationMessage(BaseMessage):
     exchange_name = 'client_activation_exchange'
     routing_key = 'client_activation_queue'
 
 
-@dataclass
+@dataclass(init=False)
 class OwnerAccessMessage(BaseMessage):
     exchange_name = 'owner_access_exchange'
     routing_key = 'owner_access_queue'
 
 
-@dataclass
+@dataclass(init=False)
 class DevAccessMessage(BaseMessage):
     exchange_name = 'dev_access_exchange'
     routing_key = 'dev_access_queue'
 
 
-@dataclass
+@dataclass(init=False)
 class FriendAddedMessage(BaseMessage):
     exchange_name = 'friend_added_exchange'
     routing_key = 'friend_added_queue'
 
 
-@dataclass
+@dataclass(init=False)
 class DevTOTPTokenMessage(BaseMessage):
     exchange_name = 'dev_totp_exchange'
     routing_key = 'dev_totp_queue'
