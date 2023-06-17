4e0d5ae4e60ad81e9c596a458f9f80a8a7b3d5bb
oxpaoff <1985oxpa@gmail.com>
2023-06-17 14:46:25 +0500

    Added user status exception

--- backend/users/backend/base/exceptions.py
+++ backend/users/backend/base/exceptions.py
@@ -1,5 +1,14 @@
 from rest_framework.exceptions import APIException
+from rest_framework import status
 
 
 class AuthenticationFailed(APIException):
-    status_code = 401
+    status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class UserInActive(Exception):
+    pass
+
+
+class UserBanned(Exception):
+    pass
