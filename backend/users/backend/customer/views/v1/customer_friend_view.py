28697f1f5332e6057714c9539cc5bd90b17fc802
kindevelopment <madeinbosco@inbox.ru>
2023-06-18 19:28:30 +0300

    Added functions for Additions, Acceptance, Deviations, Deletions request friend

--- backend/users/backend/customer/views/v1/customer_friend_view.py
+++ backend/users/backend/customer/views/v1/customer_friend_view.py
@@ -1,49 +1,18 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.db import transaction
 from django.db.models import Q
 from django.utils.decorators import method_decorator
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import generics, filters, status
+from rest_framework import generics, filters, status, viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from common.permissions.permissons import IsCustomerScopeUserPerm
+from common.permissions import permissons
+from common.permissions import verifiers
 from customer.models import CustomerUser, FriendShipRequest
 from customer.serializers.v1 import friends_serializer
 from customer.services.get_query_friend import get_queryset_for_delete_user
-from customer.services.pagination import PaginationFriend
-
-
-class FriendsView(generics.ListCreateAPIView):
-    permission_classes = [
-        IsCustomerScopeUserPerm,
-    ]
-    serializer_class = friends_serializer.UserSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ('username',)
-    http_method_names = ['get', 'post']
-    pagination_class = PaginationFriend
-
-    def get_queryset(self):
-        requests_friend = FriendShipRequest.objects.filter(
-            receiver_id=self.request.user.id, status='REQUESTED'
-        ).values_list('sender', flat=True)
-        return CustomerUser.objects.filter(is_active=True, is_banned=False, id__in=requests_friend)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_create_serializer()(
-            data=request.data, context=self.get_serializer_context()
-        )
-        serializer.is_valid(raise_exception=True)
-        with transaction.atomic():
-            serializer.save(
-                receiver_user=self.request.user,
-                sender_user=serializer.validated_data['sender_user'],
-            )
-            headers = self.get_success_headers(serializer.data)
-            return Response(status=status.HTTP_200_OK, headers=headers)
-
-    @staticmethod
-    def get_create_serializer():
-        raise NotImplementedError
+from base.paginations import BasePagination
 
 
 @method_decorator(
@@ -57,6 +26,51 @@ class FriendsView(generics.ListCreateAPIView):
         },
     ),
 )
+class GetUsersList(generics.ListAPIView):
+    serializer_class = friends_serializer.UserSerializer
+    permission_classes = (permissons.IsCustomerScopeUserPerm,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('username',)
+    http_method_names = ['get']
+    pagination_class = BasePagination
+
+    def get_queryset(self):
+        friends = self.request.user.friends.all() | CustomerUser.objects.filter(
+            friends=self.request.user
+        )
+        return CustomerUser.objects.filter(is_active=True, is_banned=False).exclude(
+            Q(id=self.request.user.id) | Q(id__in=friends.values_list('id', flat=True))
+        )
+
+
+@method_decorator(
+    name='get',
+    decorator=swagger_auto_schema(
+        operation_description='Получение пользователя для детального просмотра',
+        tags=['Пользователь', 'Друзья'],
+        responses={
+            200: openapi.Response('Пользователь', friends_serializer.UserRetrieveSerializer),
+            401: openapi.Response('Не аутентифицированный пользователь'),
+        },
+    ),
+)
+class GetUserRetrieve(generics.RetrieveAPIView):
+    serializer_class = friends_serializer.UserRetrieveSerializer
+    permission_classes = (permissons.IsCustomerScopeUserPerm,)
+    filter_backends = [filters.SearchFilter]
+    http_method_names = ['get']
+    pagination_class = BasePagination
+    lookup_url_kwarg = 'user_id'
+
+    def get_queryset(self):
+        friends = self.request.user.friends.all() | CustomerUser.objects.filter(
+            friends=self.request.user
+        )
+        return CustomerUser.objects.filter(is_active=True, is_banned=False).exclude(
+            Q(id=self.request.user.id) | Q(id__in=friends.values_list('id', flat=True))
+        )
+
+
 @method_decorator(
     name='post',
     decorator=swagger_auto_schema(
@@ -70,7 +84,16 @@ class FriendsView(generics.ListCreateAPIView):
         },
     ),
 )
-class AddFriendsView(FriendsView):
+class AddFriendsView(generics.CreateAPIView):
+    permission_classes = [
+        permissons.IsCustomerScopeUserPerm,
+    ]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('username',)
+    http_method_names = ['post']
+    pagination_class = BasePagination
+    lookup_url_kwarg = 'user_id'
+
     def get_queryset(self):
         friends = self.request.user.friends.all() | CustomerUser.objects.filter(
             friends=self.request.user
@@ -81,7 +104,7 @@ class AddFriendsView(FriendsView):
 
     def create(self, request, *args, **kwargs):
         serializer = friends_serializer.AddFriendSerializer(
-            data=request.data, context=self.get_serializer_context()
+            data=kwargs, context=self.get_serializer_context()
         )
         serializer.is_valid(raise_exception=True)
         with transaction.atomic():
@@ -90,6 +113,7 @@ class AddFriendsView(FriendsView):
             send_message_in_rabbitmq = True
             if send_message_in_rabbitmq:
                 return Response(status=status.HTTP_200_OK, headers=headers)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @method_decorator(
@@ -103,8 +127,35 @@ class AddFriendsView(FriendsView):
         },
     ),
 )
+class GetListAcceptAddFriendsView(generics.ListAPIView):
+    permission_classes = [
+        permissons.IsCustomerScopeUserPerm,
+    ]
+    serializer_class = friends_serializer.UserSerializer
+    filter_backends = [filters.SearchFilter]
+    http_method_names = ['get']
+    pagination_class = BasePagination
+
+    def get_queryset(self):
+        requests_friend = FriendShipRequest.objects.filter(
+            receiver_id=self.request.user.id, status='REQUESTED'
+        ).values_list('sender', flat=True)
+        return CustomerUser.objects.filter(is_active=True, is_banned=False, id__in=requests_friend)
+
+
 @method_decorator(
-    name='post',
+    name='retrieve',
+    decorator=swagger_auto_schema(
+        operation_description='Просмотр пользователя на добавление в друзья',
+        tags=['Пользователь', 'Друзья'],
+        responses={
+            200: openapi.Response('Пользователь', friends_serializer.UserRetrieveSerializer),
+            401: openapi.Response('Не аутентифицированный пользователь'),
+        },
+    ),
+)
+@method_decorator(
+    name='create',
     decorator=swagger_auto_schema(
         request_body=friends_serializer.AddFriendSerializer,
         operation_description='Принятие запроса для добавления в друзья',
@@ -116,25 +167,8 @@ class AddFriendsView(FriendsView):
         },
     ),
 )
-class AcceptAddFriendsView(FriendsView):
-    @staticmethod
-    def get_create_serializer():
-        return friends_serializer.AcceptAddFriendSerializer
-
-
 @method_decorator(
-    name='get',
-    decorator=swagger_auto_schema(
-        operation_description='Получение списка запросов на добавление в друзья',
-        tags=['Пользователь', 'Друзья'],
-        responses={
-            200: openapi.Response('Список запросов', friends_serializer.UserSerializer),
-            401: openapi.Response('Не аутентифицированный пользователь'),
-        },
-    ),
-)
-@method_decorator(
-    name='post',
+    name='destroy',
     decorator=swagger_auto_schema(
         request_body=friends_serializer.AddFriendSerializer,
         operation_description='Отклонение запроса для добавления в друзья',
@@ -146,10 +180,54 @@ class AcceptAddFriendsView(FriendsView):
         },
     ),
 )
-class RejectAddFriendsView(FriendsView):
-    @staticmethod
-    def get_create_serializer():
-        return friends_serializer.RejectAddFriendSerializer
+class RetrieveAcceptRejectAddFriendsView(viewsets.ModelViewSet):
+    permission_classes = [
+        permissons.IsCustomerScopeUserPerm,
+    ]
+    serializer_classes_by_action = {
+        'retrieve': friends_serializer.UserRetrieveSerializer,
+        'create': friends_serializer.AcceptAddFriendSerializer,
+        'destroy': friends_serializer.RejectAddFriendSerializer,
+    }
+    filter_backends = [filters.SearchFilter]
+    http_method_names = ['get', 'post', 'delete']
+    pagination_class = BasePagination
+    lookup_url_kwarg = 'user_id'
+
+    def get_serializer(self, *args, **kwargs):
+        try:
+            serializer_class = self.serializer_classes_by_action[self.action]
+        except KeyError:
+            serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
+
+    def get_queryset(self):
+        requests_friend = FriendShipRequest.objects.filter(
+            receiver_id=self.request.user.id, status='REQUESTED'
+        ).values_list('sender', flat=True)
+        return CustomerUser.objects.filter(is_active=True, is_banned=False, id__in=requests_friend)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=kwargs, context=self.get_serializer_context())
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+            serializer.save(
+                receiver_user=self.request.user,
+                sender_user=serializer.validated_data['sender_user'],
+            )
+            headers = self.get_success_headers(serializer.data)
+            return Response(status=status.HTTP_200_OK, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=kwargs, context=self.get_serializer_context())
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+            serializer.save(
+                receiver_user=self.request.user,
+                sender_user=serializer.validated_data['sender_user'],
+            )
+            return Response(status=status.HTTP_200_OK)
 
 
 @method_decorator(
@@ -163,24 +241,67 @@ class RejectAddFriendsView(FriendsView):
         },
     ),
 )
+class GetAllFriendsView(generics.ListAPIView):
+    permission_classes = [
+        permissons.IsCustomerScopeUserPerm,
+    ]
+    serializer_class = friends_serializer.UserSerializer
+    filter_backends = [filters.SearchFilter]
+    http_method_names = ['get']
+    pagination_class = BasePagination
+
+    def get_queryset(self):
+        friend = get_queryset_for_delete_user(self.request.user)
+        return CustomerUser.objects.filter(is_active=True, is_banned=False, id__in=friend)
+
+
 @method_decorator(
-    name='post',
+    name='get',
     decorator=swagger_auto_schema(
-        request_body=friends_serializer.AddFriendSerializer,
+        operation_description='Детальный просмотр друга',
+        tags=['Пользователь', 'Друзья'],
+        responses={
+            200: openapi.Response('Пользователь', friends_serializer.UserRetrieveSerializer),
+            401: openapi.Response('Не аутентифицированный пользователь'),
+        },
+    ),
+)
+@method_decorator(
+    name='destroy',
+    decorator=swagger_auto_schema(
+        request_body=friends_serializer.DeleteFriendSerializer,
         operation_description='Удаление друзей из списка',
         tags=['Пользователь', 'Друзья'],
         responses={
-            200: openapi.Response('Запрос на удаление отправлен'),
+            200: openapi.Response('Пользователь удалён'),
             400: openapi.Response('Данные не валидны'),
             401: openapi.Response('Не аутентифицированный пользователь'),
         },
     ),
 )
-class DeleteFriendsView(FriendsView):
+class RetrieveDestroyFriendsView(generics.RetrieveDestroyAPIView):
+    permission_classes = [
+        permissons.IsCustomerScopeUserPerm,
+    ]
+    filter_backends = [filters.SearchFilter]
+    http_method_names = ['get', 'delete']
+    search_fields = ('username',)
+    serializer_class = friends_serializer.UserRetrieveSerializer
+    pagination_class = BasePagination
+    lookup_url_kwarg = 'user_id'
+
     def get_queryset(self):
         friend = get_queryset_for_delete_user(self.request.user)
         return CustomerUser.objects.filter(is_active=True, is_banned=False, id__in=friend)
 
-    @staticmethod
-    def get_create_serializer():
-        return friends_serializer.DeleteFriendSerializer
+    def destroy(self, request, *args, **kwargs):
+        serializer = friends_serializer.DeleteFriendSerializer(
+            data=kwargs, context=self.get_serializer_context()
+        )
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+            serializer.save(
+                receiver_user=self.request.user,
+                sender_user=serializer.validated_data['sender_user'],
+            )
+            return Response(status=status.HTTP_200_OK)
