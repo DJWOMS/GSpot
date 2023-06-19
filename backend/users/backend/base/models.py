11b0c1ec6b5ba93894eb106bff3ace5794f2de87
Vladislav Fadeev <a9144199959@mail.ru>
2023-06-19 21:10:31 +0800

    .

--- backend/users/backend/base/models.py
+++ backend/users/backend/base/models.py
@@ -10,8 +10,6 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
-from administrator.models import Admin
-
 
 class BaseAbstractUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
