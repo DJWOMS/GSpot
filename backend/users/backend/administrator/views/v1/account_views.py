9dd11ce102cbd938dfd1e60917ad958ac1586f85
Kimchideveloper <daniilcehmestrenko@icloud.com>
2023-06-19 20:59:12 +0300

    refactorin endpoints

--- backend/users/backend/administrator/views/v1/account_views.py
+++ backend/users/backend/administrator/views/v1/account_views.py
@@ -11,6 +11,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+
 
 @method_decorator(
     name="retrieve",
