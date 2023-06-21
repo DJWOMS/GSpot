8842c65a2ff35aacf35fe618561de065bc031ed3
Criativve <35040312+Criativve@users.noreply.github.com>
2023-06-21 22:40:36 +0300

    Payments | New api -- item-purchase-history   (#325)
    * Added default admin panel
    
    * added api - item-purchase-history
    
    * fix by linters
    
    * fixed issues in api - item-purchase-history
    
    * fixed issues in api - item-purchase-history
    
    ---------
    
    Co-authored-by: koval.dmitriy <Vjkj3bkmyjtzqwj>

--- backend/payments/apps/base/serializer.py
+++ backend/payments/apps/base/serializer.py
@@ -1,5 +1,5 @@
-from apps.base.fields import MoneyAmountSerializerField
 from apps.base.schemas import EnumCurrencies, PaymentServices, PaymentTypes
+from django.conf import settings
 from rest_enumfield import EnumField
 from rest_framework import serializers
 
@@ -10,5 +10,10 @@ class PaymentServiceSerializer(serializers.Serializer):
 
 
 class MoneySerializer(serializers.Serializer):
-    amount = MoneyAmountSerializerField()
+    # amount = MoneyAmountSerializerField()
+    amount = serializers.DecimalField(
+        min_value=0,
+        max_digits=settings.MAX_BALANCE_DIGITS,
+        decimal_places=2,
+    )
     currency = EnumField(choices=EnumCurrencies)
--- backend/payments/apps/item_purchases/urls.py
+++ backend/payments/apps/item_purchases/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('purchase/', views.PurchaseItemView.as_view({'post': 'create'})),
     path('refund/', views.RefundView.as_view({'post': 'create'})),
+    path(
+        'item-purchase-history/<uuid:user_uuid>/',
+        views.ItemPurchaseHistoryView.as_view({'get': 'list'}),
+    ),
 ]
