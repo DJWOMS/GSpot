4649f16ae5ba0700bcabcb4b8536ba0023c2218b
koval.dmitriy <Vjkj3bkmyjtzqwj>
2023-06-25 12:12:16 +0300

    fixed CommissionField

--- backend/payments/apps/external_payments/models.py
+++ backend/payments/apps/external_payments/models.py
@@ -30,15 +30,7 @@ class PaymentCommission(models.Model):
 
     payment_service_id = models.ForeignKey(PaymentService, on_delete=models.CASCADE)
     payment_type = models.CharField(max_length=50, verbose_name='type_of_payment')
-    commission = CommissionField(
-        validators=(
-            MinValueValidator(0, message='Should be positive value'),
-            MaxValueValidator(
-                MAX_COMMISSION,
-                message=f'Should be not greater than {MAX_COMMISSION}',
-            ),
-        ),
-    )
+    commission = CommissionField()
 
     def __str__(self):
         return f'Type of payment: {self.payment_type}' f'Commission amount: {self.commission}'
