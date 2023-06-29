e6fcb9e9996d846d749ba549264e3426789f85e8
Evgenii <evgeniirg73@gmail.com>
2023-06-29 22:06:23 +0700

    some fix

--- backend/payments/apps/payment_accounts/signals.py
+++ backend/payments/apps/payment_accounts/signals.py
@@ -13,16 +13,13 @@ from .tasks import make_auto_payout
 def update_celery_schedule(sender, instance, **kwargs):
     try:
         payout_day = instance.payout_day_of_month
-        print(f'--day--{payout_day}')
         task_name = 'make_auto_payout'
         if task_name in app.conf.beat_schedule:
-            print(f'вот расписание {app.conf.beat_schedule}')
             del app.conf.beat_schedule[task_name]
         app.conf.beat_schedule[task_name] = {
             'task': 'apps.payment_accounts.tasks.make_auto_payout',
             'schedule': crontab(day_of_month=payout_day),
         }
-        print(f'новое {app.conf.beat_schedule[task_name]}')
         current_date = date.today()
         if current_date.day == payout_day:
             make_auto_payout.apply_async()
