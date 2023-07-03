37eb9b519d9e5adf84d9449e659862ac43e08925
Roman-Zhirovskis <tesslogun@gmail.com>
2023-07-03 16:24:45 +0300

    fix issues from coments

--- backend/users/backend/common/serializers/v1/change_password_serializer.py
+++ backend/users/backend/common/serializers/v1/change_password_serializer.py
@@ -1,20 +1,15 @@
-from developer.models import CompanyUser
+from base.models import BaseAbstractUser
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
-class ChangePasswordSerializers(serializers.ModelSerializer):
+class ChangePasswordSerializers(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True, validators=[validate_password])
     confirmation_new_password = serializers.CharField(required=True)
 
-    class Meta:
-        ref_name = "developer_account_change_pass"
-        model = CompanyUser
-        fields = ("old_password", "new_password", "confirmation_new_password")
-
     def validate(self, attrs):
-        user: CompanyUser = self.context["request"].user
+        user: BaseAbstractUser = self.context["request"].user
         if not user.check_password(attrs["old_password"]):
             raise serializers.ValidationError("Invalid old password")
 
@@ -23,6 +18,6 @@ class ChangePasswordSerializers(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data) -> None:
-        instance: CompanyUser = self.context["request"].user
+        instance: BaseAbstractUser = self.context["request"].user
         instance.set_password(validated_data["new_password"])
         instance.save()
