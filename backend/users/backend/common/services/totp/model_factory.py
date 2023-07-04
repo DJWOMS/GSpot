d32426b926332b4fecca36d5d98bb3dbb7abcea5
Vladislav Fadeev <a9144199959@mail.ru>
2023-07-04 16:01:32 +0800

    Users - TOTP serializers fix

--- backend/users/backend/common/services/totp/model_factory.py
+++ backend/users/backend/common/services/totp/model_factory.py
@@ -1,32 +1,29 @@
-from rest_framework.serializers import ModelSerializer
-
 from administrator.models import Admin
 from base.models import BaseAbstractUser
 from customer.models import CustomerUser
 from developer.models import CompanyUser
-from administrator.models import Admin
-
+from rest_framework.serializers import ModelSerializer
 
 
 class CustomerTOTPSerializer(ModelSerializer):
     class Meta:
         model = CustomerUser
         field = '__all__'
-        exclude = ('password', )
+        exclude = ('password',)
 
 
 class CompanyUserTOTPSerializer(ModelSerializer):
     class Meta:
         model = CompanyUser
         field = '__all__'
-        exclude = ('password', )
+        exclude = ('password',)
 
 
 class AdminTOTPSerializer(ModelSerializer):
     class Meta:
         model = Admin
         field = '__all__'
-        exclude = ('password', )
+        exclude = ('password',)
 
 
 class DBModelFactory:
@@ -34,7 +31,9 @@ class DBModelFactory:
         self._models = {}
         self._serializers = {}
 
-    def register(self, app_label: str, model: BaseAbstractUser, serializer: ModelSerializer) -> None:
+    def register(
+        self, app_label: str, model: BaseAbstractUser, serializer: ModelSerializer,
+    ) -> None:
         if not issubclass(model, BaseAbstractUser):
             raise TypeError('model must will be the "BaseAbstractUser" instance required!')
         self._models[app_label] = (model, serializer)
@@ -44,7 +43,7 @@ class DBModelFactory:
         if not model:
             raise ValueError(f'Models for "{app_label}" is not registered!')
         return model
-    
+
 
 db_model_factory = DBModelFactory()
 db_model_factory.register('customer', CustomerUser, CustomerTOTPSerializer)
