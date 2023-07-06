5db46c93d93b2e300653d4832db2aa2c54849bbd
koval.dmitriy <Vjkj3bkmyjtzqwj>
2023-07-06 23:14:21 +0300

    fixed linters

--- backend/payments/apps/external_payments/migrations/0002_alter_paymentcommission_unique_together.py
+++ backend/payments/apps/external_payments/migrations/0002_alter_paymentcommission_unique_together.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('external_payments', '0001_initial'),
     ]
--- backend/payments/apps/external_payments/signals.py
+++ backend/payments/apps/external_payments/signals.py
@@ -1,12 +1,9 @@
+from apps.external_payments.models import PaymentCommission
 from django.core.cache import cache
 from django.db import models
 from django.dispatch import receiver
-
-from apps.external_payments.models import PaymentCommission
 
 
 @receiver(models.signals.post_save, sender=PaymentCommission)
 def update_cache(sender, instance: PaymentCommission, **kwargs):
     cache.set(instance.cache_key, instance.commission)
-
-
