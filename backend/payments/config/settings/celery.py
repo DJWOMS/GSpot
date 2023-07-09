5b33579899032cc4c06e5d5682e1c0ae840c6d8b
koval.dmitriy <Vjkj3bkmyjtzqwj>
2023-07-09 22:33:56 +0300

    fixed flake

--- backend/payments/config/settings/celery.py
+++ backend/payments/config/settings/celery.py
@@ -8,6 +8,6 @@ CELERY_BROKER_URL = REDIS_URL + '0'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': REDIS_URL + '0'
+        'LOCATION': REDIS_URL + '0',
     },
 }
