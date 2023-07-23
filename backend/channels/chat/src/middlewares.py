2cbcefc5147807b551b09c5318f387119e5639c3
Vasya <oiuy78@yandex.ru>
2023-07-23 17:56:22 +0300

    User participant permission

--- backend/channels/chat/src/middlewares.py
+++ backend/channels/chat/src/middlewares.py
@@ -1,5 +1,6 @@
 import aiohttp
 from fastapi import HTTPException
+from src.rooms.services import get_room_participant
 from starlette.status import HTTP_403_FORBIDDEN
 
 
@@ -7,7 +8,7 @@ async def get_token():
     pass
 
 
-async def get_user_or_403(token: str):
+async def get_user_or_403(token: str) -> None | HTTPException:
     async with aiohttp.ClientSession() as session:
         async with session.get(
             'http://users.alpha.g-spot.website/api/v1/customer/customer/me',
@@ -16,3 +17,10 @@ async def get_user_or_403(token: str):
             if resp.status != 200:
                 raise HTTPException(HTTP_403_FORBIDDEN)
     return
+
+
+async def user_is_participant(room_id: str, user_id: str) -> None | HTTPException:
+    """'get_room_participant` return `RoomParticipant` object or None"""
+    if await get_room_participant(room_id, user_id):
+        return
+    raise HTTPException(HTTP_403_FORBIDDEN)
--- backend/channels/chat/src/rooms/services.py
+++ backend/channels/chat/src/rooms/services.py
@@ -9,3 +9,11 @@ async def get_room_messages(room_id: str) -> List[Dict[str, Any] | None]:
     async for message in collection:
         messages.append(message)
     return messages
+
+
+async def get_room_participant(room_id: str, user_id: str) -> List[Dict[str, Any] | None]:
+    user = (
+        await db.client.your_mongodb_db.get_collection('room_participant').find_one({
+            'room_id': room_id, 'user_id': user_id})
+    )
+    return user
