f00204f3ffd2e1fea065c90721807d85b285d290
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-07-23 23:11:44 +0300

    added async sending emails

--- backend/channels/broker/consumers/email/consumer.py
+++ backend/channels/broker/consumers/email/consumer.py
@@ -1,9 +1,16 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from aio_pika.message import IncomingMessage
-from ..abstract import RabbitMQConsumer
-from aio_pika.queue import Queue
+import json
 import logging
+from email.message import EmailMessage
 
+import aiosmtplib
+from aio_pika.message import IncomingMessage
+from aio_pika.queue import Queue
+from config.smtp import smtp_config
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic.error_wrappers import ValidationError
+
+from ..abstract import RabbitMQConsumer
+from .models import Mail
 
 logger = logging.getLogger(__name__)
 
@@ -11,16 +18,35 @@ logger = logging.getLogger(__name__)
 class EmailConsumer(RabbitMQConsumer):
     queue_name = 'email'
 
-    def __int__(self, queue: Queue, db_client: AsyncIOMotorClient):
+    def __init__(self, queue: Queue, db_client: AsyncIOMotorClient):
         super().__init__(
             queue=queue,
-            # db_client=db_client
+            db_client=db_client
         )
 
     async def process_message(self, orig_message: IncomingMessage):
         async with orig_message.process():
-            print('сообщение отработано и успешно удалено из очереди в консьюмере mail')
-        # await self.db_client.do_insert(orig_message)
-        logger.info(orig_message.body)
+            try:
+                message = json.loads(orig_message.body)
+                mail = Mail(**message)
 
+                smtp_client = aiosmtplib.SMTP(
+                    hostname=smtp_config.SMTP_HOST,
+                    port=smtp_config.SMTP_PORT,
+                    use_tls=True
+                )
 
+                message = EmailMessage()
+                message['Subject'] = mail.subject
+                message['From'] = smtp_config.EMAIL_ADDRESS
+                message['To'] = mail.email
+                message.set_content(mail.body, subtype='html')
+
+                async with smtp_client:
+                    await smtp_client.login(username=smtp_config.EMAIL_ADDRESS, password=smtp_config.EMAIL_PASSWORD)
+                    await smtp_client.send_message(message)
+
+                logger.info(f'Сообщение отправлено {orig_message.body}')
+
+            except (ValidationError, json.JSONDecodeError, aiosmtplib.SMTPDataError):
+                logger.error(f'Не удалось отправить сообщение {orig_message.body}')
