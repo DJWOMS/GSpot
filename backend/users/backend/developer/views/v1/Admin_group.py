0db507fc06ce490b7e5517184536af47c9e83dc4
ergashtogo <asqanabana@gmail.com>
2023-07-27 15:43:58 +0500

    Исправил

--- backend/users/backend/developer/views/v1/Admin_group.py
+++ backend/users/backend/developer/views/v1/Admin_group.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from administrator.models import AdminPermission, Admin
-from developer.serializers.v1.assign_permission_serializer import AdminPermissionSerializer
+from administrator.models import AdminGroup, Admin
+from developer.serializers.v1.assign_permission_serializer import AdminGroupSerializer
 from rest_framework.permissions import IsAdminUser
 
-class AdminPermissionViewSet(APIView):
+class AdminGroupViewSet(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request, admin_id):
@@ -14,10 +14,10 @@ class AdminPermissionViewSet(APIView):
         except Admin.DoesNotExist:
             return Response({"error": "Admin not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        permissions = admin.user_permissions.all()
-        permission_serializer = AdminPermissionSerializer(permissions, many=True)
+        groups = admin.groups.all()
+        group_serializer = AdminGroupSerializer(groups, many=True)
 
-        return Response({"permissions": permission_serializer.data}, status=status.HTTP_200_OK)
+        return Response({"groups": group_serializer.data}, status=status.HTTP_200_OK)
 
     def post(self, request, admin_id):
         try:
@@ -25,7 +25,7 @@ class AdminPermissionViewSet(APIView):
         except Admin.DoesNotExist:
             return Response({"error": "Admin not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        permission_ids = request.data.get('permission_ids', [])
-        admin.user_permissions.set(AdminPermission.objects.filter(id__in=permission_ids))
+        group_ids = request.data.get('group_ids', [])
+        admin.groups.set(AdminGroup.objects.filter(id__in=group_ids))
 
-        return Response({"message": "Permissions assigned successfully"}, status=status.HTTP_200_OK)
+        return Response({"message": "Groups assigned successfully"}, status=status.HTTP_200_OK)
